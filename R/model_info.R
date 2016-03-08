e46458ad3c876fd22adc88c3e68b207489b48b68
Benjamin <guiastrennec@gmail.com>
2016-03-08 22:07:31 +0100

    updated method function

--- R/model_info.R
+++ R/model_info.R
@@ -54,11 +54,12 @@ nind <- function(model) {
 method <- function(model) {
   string <- gsub('.*METHOD=(\\w+)\\s+.*', '\\1', model$CODE[grepl('METHOD=', model$CODE)])
   inter  <- ifelse(grepl('INTER', model$CODE[grepl('METHOD=', model$CODE)]), '-I', '')
+  lapl   <- ifelse(grepl('LAPL', model$CODE[grepl('METHOD=', model$CODE)]), ' Laplace', '')
 
   if (any(grepl('\\d', string))) {
     string[grepl('\\d', string)] <- c('FO', 'FOCE')[as.numeric(string[grepl('\\d', string)]) + 1]
   }
-  string <- paste0(string, inter)
+  string <- paste0(string, inter, lapl)
   string <- paste('method:', paste(string, collapse = ', '))
   return(string)
 }
