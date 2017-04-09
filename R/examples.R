497401cb6c5a7f6cd2197ecde3f0abc168ee2187
Benjamin <guiastrennec@gmail.com>
2017-04-09 22:24:40 +0200

    Added pkgdown website

---
+++ R/examples.R
@@ -0,0 +1,18 @@
+#' \code{xpose_data} examples
+#'
+#' @description Moxonidine \code{xpose_data} example.
+#'
+#' @format An \code{xpose_data} object
+#'
+#' @source \href{http://link.springer.com/article/10.1023/A:1020561807903}{Moxonidine model}: 
+#' Assumption Testing in Population Pharmacokinetic Models: Illustrated with an Analysis of 
+#' Moxonidine Data from Congestive Heart Failure Patients. Journal of Pharmacokinetics and 
+#' Biopharmaceutics. 26(2):207–246 (1998).
+#'
+#' @examples
+#' \dontrun{
+#' dv_vs_ipred(xpdb_ex_pk)
+#' }
+#'
+#' @name xpdb_ex_pk
+NULL
