0fa37f1b656ce1f39322783e1446b5697b587b16
Benjamin <guiastrennec@gmail.com>
2017-04-10 23:57:31 +0200

    improve test portability

--- tests/testthat/test-xpose_save.R
+++ tests/testthat/test-xpose_save.R
@@ -9,7 +9,8 @@ test_that("errors are returned for bad plot input", {
 
 
 test_that("errors are returned for bad filename input", {
-  paths_1 <- paste0(tempdir(), '/test_plot.', c('abcd', 'bcde'))
+  paths_1 <- file.path(tempdir(), 
+                       paste0('test_plot.', c('abcd', 'bcde')))
   on.exit(unlink(paths_1))
   
   # Unrecognized extension
@@ -24,15 +25,15 @@ test_that("errors are returned for bad filename input", {
 
 
 test_that("all graphical devices work properly", {
-  paths_2 <- paste0(tempdir(), '/test_plot.', 
-                    c('pdf', 'jpeg', 'png', 'bmp', 'tiff'))
+  paths_2 <- file.path(tempdir(), 
+                       paste0('test_plot.', c('pdf', 'jpeg', 'png', 'bmp', 'tiff')))
   on.exit(unlink(paths_2))
   
   expect_false(any(file.exists(paths_2)))
-  xpose_save(plot = plot, filename = paths_2[1]) # pdf
-  xpose_save(plot = plot, filename = paths_2[2]) # jpeg
-  xpose_save(plot = plot, filename = paths_2[3]) # png
-  xpose_save(plot = plot, filename = paths_2[4]) # bmp
-  xpose_save(plot = plot, filename = paths_2[5]) # tiff
+  xpose_save(plot = plot, filename = paths_2[1], res = 20) # pdf
+  xpose_save(plot = plot, filename = paths_2[2], res = 20) # jpeg
+  xpose_save(plot = plot, filename = paths_2[3], res = 20) # png
+  xpose_save(plot = plot, filename = paths_2[4], res = 20) # bmp
+  xpose_save(plot = plot, filename = paths_2[5], res = 20) # tiff
   expect_true(all(file.exists(paths_2)))
 })
