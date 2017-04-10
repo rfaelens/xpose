7bafa035ae98a41a7f89b2d4c894fbb08c35308f
Benjamin <guiastrennec@gmail.com>
2017-04-10 14:43:54 +0200

    Added vignettes

--- vignettes/customizing_plots.R
+++ vignettes/customizing_plots.R
@@ -9,7 +9,7 @@ knitr::opts_chunk$set(fig.dpi = 96,
                       fig.width = 4,
                       out.width = '75%')
 
-## ----demo type, echo = FALSE, fig.width = 6, out.width = '100%'----------
+## ----demo type, echo = FALSE, fig.height = 2.5, fig.width = 6, out.width = '100%'----
 gridExtra::grid.arrange(
   dv_vs_ipred(xpdb, title = "type = \'p\'", subtitle = FALSE, caption = FALSE, type = 'p'),
   dv_vs_ipred(xpdb, title = "type = \'l\'", subtitle = FALSE, caption = FALSE, type = 'l'),
@@ -44,24 +44,24 @@ dv_vs_ipred(xpdb, type = 'p',
 ## ----demo panels---------------------------------------------------------
 dv_vs_ipred(xpdb, by = 'SEX')
 
-## ----demo layers, eval = FALSE-------------------------------------------
-#  dv_vs_ipred(xpdb) +
-#    geom_rug(alpha = 0.2, color = 'grey50',
-#             sides = 'lb', size = 0.4) +
-#    annotate(geom = 'text',
-#             color = 'darkred',
-#             label = 'LLOQ', x = -4, y = -4) +
-#    annotate(geom = 'rect',
-#             alpha = 0.2, fill = 'red',
-#             xmin = -Inf, xmax = -3,
-#             ymin = -Inf, ymax = -3)
+## ----demo layers---------------------------------------------------------
+dv_vs_ipred(xpdb) +
+  geom_rug(alpha = 0.2, color = 'grey50',
+           sides = 'lb', size = 0.4) +
+  annotate(geom = 'text', 
+           color = 'darkred',
+           label = 'LLOQ', x = -4, y = -4) +
+  annotate(geom = 'rect',
+           alpha = 0.2, fill = 'red',
+           xmin = -Inf, xmax = -3,
+           ymin = -Inf, ymax = -3)
 
 ## ----scales demo---------------------------------------------------------
 dv_vs_ipred(xpdb, 
             xscale_breaks = c(-4, -2, 0),
             xscale_labels = c('Low', 'Med', 'High'),
             xscale_expand = c(0.2, 0),
-            xscale_name = 'Individual model prediction (mg/L)')
+            xscale_name = 'Individual model prediction')
 
 ## ----demo themes xpdb, eval = FALSE--------------------------------------
 #  # While creating the xpdb
@@ -78,11 +78,22 @@ dv_vs_ipred(xpdb,
 ## ----demo themes plot----------------------------------------------------
 # For a single plot only
 dv_vs_ipred(xpdb        = xpdb, 
-            gg_theme    = ggplot2::theme_dark(), 
+            gg_theme    = theme_readable(), 
             point_color = 'darkblue')
 
-## ---- eval = FALSE-------------------------------------------------------
-#  xpose_data(runno = '001') %>%
-#    dv_vs_ipred() %>%
-#    xpose_save()
+## ----demo gg_theme, echo = FALSE, fig.height = 6, fig.width = 6, out.width = '100%'----
+gridExtra::grid.arrange(
+  dv_vs_ipred(xpdb, subtitle = 'gg_theme = theme_readable()', title = FALSE, caption = '', gg_theme = theme_readable()),
+  dv_vs_ipred(xpdb, subtitle = 'gg_theme = theme_grey()', title = FALSE, caption = '', gg_theme = theme_grey()),
+  dv_vs_ipred(xpdb, subtitle = 'gg_theme = theme_bw2()', title = FALSE, caption = FALSE, gg_theme = theme_bw2()),
+  dv_vs_ipred(xpdb, subtitle = 'gg_theme = theme_dark()', title = FALSE, caption = FALSE, gg_theme = theme_dark()),
+  ncol = 2)
+
+## ----demo xp_theme, echo = FALSE, fig.height = 4, fig.width = 6, out.width = '100%'----
+gridExtra::grid.arrange(
+  dv_vs_ipred(xpose_theme(xpdb = xpdb, xp_theme = xp_theme_default()),
+              subtitle = 'xp_theme = xp_theme_default()', title = FALSE, caption = ''),
+  dv_vs_ipred(xpose_theme(xpdb = xpdb, xp_theme = xp_theme_xpose4()),
+              subtitle = 'xp_theme = xp_theme_xpose4()\nwith gg_theme = theme_bw2()', title = FALSE, caption = '', gg_theme = theme_bw2()),
+  ncol = 2)
 
