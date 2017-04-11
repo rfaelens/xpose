49396b43d9915a8d97e16e9b04f7a6d9e02dc027
Benjamin <guiastrennec@gmail.com>
2017-04-11 10:59:20 +0200

    broke down devices test

--- tests/testthat/test-xpose_save.R
+++ tests/testthat/test-xpose_save.R
@@ -24,22 +24,53 @@ test_that("errors are returned for bad filename input", {
 })
 
 
-test_that("all graphical devices work properly", {
-  tmp_dr <- tempdir()
-  paths_2 <- file.path(tmp_dr, 
-                       paste0('test_plot.', c('pdf', 'jpeg', 'png', 'bmp', 'tiff')))
+test_that("pdf graphical device works properly", {
+  paths_2 <- file.path(tempdir(), paste0('test_plot.pdf'))
   on.exit(unlink(paths_2))
+
+  xpose_save(plot = plot, filename = paths_2)               # pdf
+  expect_true(file.exists(paths_2))
+})
+
+test_that("jpeg graphical device works properly", {
+  paths_3 <- file.path(tempdir(), paste0('test_plot.jpeg'))
+  on.exit(unlink(paths_3))
   
-  print(paths_2)
+  xpose_save(plot = plot, filename = paths_3, res = 20)    # jpeg
   
-  expect_true(dir.exists(tmp_dr))
+  expect_true(file.exists(paths_3))
+})
+
+test_that("png graphical device works properly", {
+  paths_4 <- file.path(tempdir(), paste0('test_plot.png'))
+  on.exit(unlink(paths_4))
   
+  xpose_save(plot = plot, filename = paths_4, res = 20)    # png
+  
+  expect_true(file.exists(paths_4))
+})
+
+test_that("bmp graphical device works properly", {
+  paths_5 <- file.path(tempdir(), paste0('test_plot.bmp'))
+  on.exit(unlink(paths_5))
+  
+  xpose_save(plot = plot, filename = paths_5, res = 20)    # bmp
+  
+  expect_true(file.exists(paths_5))
+})
+
+test_that("tiff graphical device works properly", {
+  paths_6 <- file.path(tempdir(), paste0('test_plot.tiff'))
+  on.exit(unlink(paths_6))
+  
+  xpose_save(plot = plot, filename = paths_6, res = 20)    # tiff
+  
+  expect_true(file.exists(paths_6))
+})
+
   #expect_false(any(file.exists(paths_2)))
-  xpose_save(plot = plot, filename = paths_2[1])           # pdf
-  expect_true(file.exists(paths_2[1]))
   #xpose_save(plot = plot, filename = paths_2[2], res = 20) # jpeg
   #xpose_save(plot = plot, filename = paths_2[3], res = 20) # png
   #xpose_save(plot = plot, filename = paths_2[4], res = 20) # bmp
   #xpose_save(plot = plot, filename = paths_2[5], res = 20) # tiff
   #expect_true(all(file.exists(paths_2)))
-})
