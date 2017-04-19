24be2ca4c4eab95dbdc884564c407eaf1452bc00
Benjamin <guiastrennec@gmail.com>
2017-04-19 10:03:28 +0200

    Cleanup code

--- R/combine_nm_tab.R
+++ R/combine_nm_tab.R
@@ -1,27 +1,26 @@
 combine_nm_tab <- function(mod_file = NULL,
                            dir      = NULL,
-                           verbose  = FALSE){
+                           verbose  = FALSE) {
   
-  # Check inputs
   if (is.null(mod_file)) {
-    stop('Argument \"mod_file\" required.', call. = FALSE)
+    stop('Argument `mod_file` required.', call. = FALSE)
   }
   
-  # Extract file name
+  # Extract tab names
   tab_file  <- unlist(sapply(strsplit(grep(pattern = '.*FILE\\s*=\\s*',
                                            x = mod_file$CODE[mod_file$ABREV == 'TAB'],
                                            value = TRUE), '.*FILE\\s*=\\s*'), '[', 2))
   
   # Ensure file exsits
-  tab_file  <- tab_file[file.exists(paste0(dir, tab_file))]
+  tab_file  <- tab_file[file.exists(file.path(dir, tab_file))]
   
   if (is.null(tab_file) || length(tab_file) == 0) {
-    stop('No output table available.', call. = FALSE)
+    msg('No output table available.', verbose)
+    return(NULL)
   } else {
     tab_out  <- read_nm_tab(file = paste0(dir, tab_file),
                             rm_duplicates = TRUE,
                             index = TRUE)
   }
-  return(tab_out)
-  
+  tab_out
 }
--- R/read_nm_tab.R
+++ R/read_nm_tab.R
@@ -7,14 +7,12 @@
 #' provided for a same NONMEM run, they will be combined into
 #' a single \code{data.frame}.
 #'
-#' @param file Full file name.
+#' @param file Path to the file.
 #' @param skip Number of lines to skip before reading data.
-#' @param header Logical value indicating whether the file has header or not.
+#' @param header Logical value indicating whether the file has a header row or not.
 #' @param rm_duplicates Logical value indicating whether duplicated columns should be removed.
-#' @param nonmem_tab Logical value indicading to the function whether the file is a
-#' table or an additional NONMEM output file.
-#' @param index Logical, when \code{nonmem_tab} is \code{TRUE} returns a list with the data and an
-#' index of all filenames and their colnames.
+#' @param index Logical value indiacating whether the data should be returned as a simple data.frame or 
+#' a list containing the data and an index of the colum names.
 #'
 #' @examples
 #' \dontrun{
@@ -25,12 +23,10 @@ read_nm_tab <- function(file = NULL,
                         skip = NULL,
                         header = NULL,
                         rm_duplicates = FALSE,
-                        nonmem_tab = TRUE,
                         index = FALSE) {
   
-  # Check inputs
   if (is.null(file)) {
-    stop('Argument \"file\" required.', call. = FALSE)
+    stop('Argument `file` required.', call. = FALSE)
   }
   
   if (!any(file.exists(file))) {
@@ -39,42 +35,29 @@ read_nm_tab <- function(file = NULL,
     file <- file[file.exists(file)]
   }
   
-  if (nonmem_tab) {
-    # If auto mode required
-    if (is.null(skip) & is.null(header)) {
-      test    <- readLines(file[1], n = 3)
-      skip    <- ifelse(grepl('TABLE NO', test[1]), 1, 0)
-      header  <- ifelse(grepl('[a-zA-Z]', test[2]), TRUE, FALSE)
-    }
-    
-    # Import data
-    raw   <- lapply(file, readr::read_table, skip = skip, 
-                    col_names = header, col_types = readr::cols())
-    
-    # Index datasets
-    if (index) {
-      index_dat <- lapply(raw, colnames)
-      names(index_dat) <- basename(file)
-    }
-    
-    # Compact data
-    tab_file <- do.call('cbind', raw)
-    tab_file <- as.data.frame(apply(tab_file, MARGIN = 2, FUN = as.numeric))
-    
-    # Drop rows with NA (in simtab)
-    tab_file <- stats::na.omit(tab_file)
-    
-  } else {
-    # if nonmem_tab == FALSE
-    # Search for final results only
-    skip     <- max(grep('TABLE NO', readLines(file[1])))
-    
-    # Import all files
-    tab_file <- do.call('cbind', lapply(file, utils::read.table, skip = skip,
-                                        header = FALSE, fill = TRUE, as.is = TRUE))
-    colnames(tab_file) <- tab_file[1, ]
-    tab_file <- suppressWarnings(as.data.frame(apply(tab_file[-1, ], 2, as.numeric)))
+  # If auto mode required
+  if (is.null(skip) & is.null(header)) {
+    test    <- readLines(file[1], n = 3)
+    skip    <- ifelse(grepl('TABLE NO', test[1]), 1, 0)
+    header  <- ifelse(grepl('[a-zA-Z]', test[2]), TRUE, FALSE)
   }
+  
+  # Import data
+  raw   <- lapply(file, readr::read_table, skip = skip, 
+                  col_names = header, col_types = readr::cols())
+  
+  # Index datasets
+  if (index) {
+    index_dat <- lapply(raw, colnames)
+    names(index_dat) <- basename(file)
+  }
+  
+  # Compact data
+  tab_file <- do.call('cbind', raw)
+  tab_file <- as.data.frame(apply(tab_file, MARGIN = 2, FUN = as.numeric))
+  
+  # Drop rows with NA (in simtab)
+  tab_file <- stats::na.omit(tab_file)
   
   if (rm_duplicates) {
     tab_file <- tab_file[, !duplicated(colnames(tab_file))]
