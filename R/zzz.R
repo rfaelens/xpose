f77c35bfe6baa53ac973e9b9f3470c936ce855a1
Benjamin <guiastrennec@gmail.com>
2017-04-20 16:06:55 +0200

    Added new startup messages

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,9 @@
   
   if (!interactive()) return()
   
-  packageStartupMessage('ggxpose is under active development, for updates, checkout: https://guiastrennec.github.io/ggxpose')
+  text <- c('ggxpose is under active development, for updates, checkout: https://guiastrennec.github.io/ggxpose',
+            'Need a new plot? Suggest ideas for ggxpose on github: https://github.com/guiastrennec/ggxpose',
+            'Do you like ggxpose? Give us some feedback on github: https://github.com/guiastrennec/ggxpose')
   
+  packageStartupMessage(sample(text, size = 1))
 }
