73f10140a4af321a2a9a012294d68fff2e786aea
Benjamin <guiastrennec@gmail.com>
2017-04-23 22:57:18 +0200

    Added verbose messages

--- R/combine_nm_tab.R
+++ R/combine_nm_tab.R
@@ -6,6 +6,8 @@ combine_nm_tab <- function(mod_file = NULL,
     stop('Argument `mod_file` required.', call. = FALSE)
   }
   
+  msg('Looking for NONMEM table files.', verbose)
+  
   tab_file  <- stringr::str_match(string = mod_file$CODE[mod_file$ABREV == 'TAB'], 
                                   pattern = '\\s+FILE\\s*=\\s*([^\\s]+)')[, 2]
   tab_file  <- tab_file[!is.na(tab_file)]
@@ -13,6 +15,7 @@ combine_nm_tab <- function(mod_file = NULL,
   if (length(tab_file) == 0) {
     msg('No output table available.', verbose)
   } else {
+    msg(c('Reading:\n', paste(file_path(dir, tab_file), collapse = '\n')), verbose)
     read_nm_tab(file = file_path(dir, tab_file),
                 rm_duplicates = TRUE,
                 index = TRUE)
