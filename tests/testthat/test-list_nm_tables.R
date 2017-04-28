d61361be0098611412c58fc5b9b6faf6bdf66699
Benjamin <guiastrennec@gmail.com>
2017-04-28 10:22:12 +0200

    updated tests

--- tests/testthat/test-list_nm_tables.R
+++ tests/testthat/test-list_nm_tables.R
@@ -1,12 +1,11 @@
 context('Check list_nm_tables')
 
 # Define files to be tested -----------------------------------------------
-
-test_mod <- structure(
+simtab_test <- structure(
   dplyr::tibble(problem    = 1,
                 level      = c(19, 20, 20, 21),
-                subroutine = c('cov', rep('tab', 3)),
-                code       = c('PRINT=E', 
+                subroutine = c('sig', rep('tab', 3)),
+                code       = c('0.3', 
                                'ID OCC DOSE AMT SS II TIME TAD IPRED CWRES CPRED IWRES',
                                'EVID ONEHEADER NOPRINT FILE =my_sdtab-001a.tab',
                                'ID KA CL V ALAG1 ETAS(1:LAST) ONEHEADER NOPRINT FILE= some-tab_001.csv'),
@@ -14,35 +13,51 @@ test_mod <- structure(
   file  = 'run001.mod', dir   = NULL,
   class = c('nm_model', 'tbl_df', 'tbl', 'data.frame'))
 
-test2_mod <- structure(test_mod[1, ],
-                       file  = 'run001.mod', dir   = NULL,
-                       class = c('nm_model', 'tbl_df', 'tbl', 'data.frame'))
+fail_test1 <- structure(simtab_test[1, ],
+                        file  = 'run001.mod', dir   = NULL,
+                        class = c('nm_model', 'tbl_df', 'tbl', 'data.frame'))
 
-test3_mod <- structure(test_mod[1:2, ],
-                       file  = 'run001.mod', dir   = NULL,
-                       class = c('nm_model', 'tbl_df', 'tbl', 'data.frame'))
+fail_test2 <- structure(simtab_test[1:2, ],
+                        file  = 'run001.mod', dir   = NULL,
+                        class = c('nm_model', 'tbl_df', 'tbl', 'data.frame'))
+
+sdtab_test <- read_nm_model(file = 'run001.lst')
+
 
 # Tests start here --------------------------------------------------------
 
-test_that("error is returned when missing mod_file argument", {
+test_that("error is returned when missing nm_model argument", {
   expect_error(list_nm_tables())
 })
 
-test_that("error is returned when input is not a mod_file", {
-  expect_error(list_nm_tables(mod_file = dplyr::tibble(problem = 1, level = 10, 
+test_that("error is returned when input is not a nm_model", {
+  expect_error(list_nm_tables(nm_model = dplyr::tibble(problem = 1, level = 10, 
                                                        subroutine = 'tab', code = 'FILE=sdtab001',
                                                        comment = '')))
 })
 
 test_that("null is returned no $TABLE are listed in the code", {
-  expect_null(list_nm_tables(mod_file = test2_mod))
+  expect_null(list_nm_tables(nm_model = fail_test1))
 })
 
 test_that("null is returned no valid file names are found under $TABLE", {
-  expect_null(list_nm_tables(mod_file = test3_mod))
+  expect_null(list_nm_tables(nm_model = fail_test2))
 })
 
-test_that("returns table names when proper input is provided", {
-  expect_equal(list_nm_tables(mod_file = test_mod), 
-               c("my_sdtab-001a.tab", "some-tab_001.csv"))
+test_that("works with simulation problems", {
+  simtab_out <- list_nm_tables(nm_model = simtab_test)
+  
+  expect_true(is.nm.table.list(simtab_out))
+  expect_false(any(simtab_out$firstonly))
+  expect_true(all(simtab_out$simtab))
+  expect_equal(simtab_out$file, c("my_sdtab-001a.tab", "some-tab_001.csv"))
+})
+
+test_that("works with estimation problems", {
+  sdtab_out <- list_nm_tables(nm_model = sdtab_test)
+  
+  expect_true(is.nm.table.list(sdtab_out))
+  expect_false(any(sdtab_out$firstonly))
+  expect_false(any(sdtab_out$simtab))
+  expect_equal(sdtab_out$file, paste0('./', c('sd', 'ca', 'co', 'pa'), 'tab001'))
 })
