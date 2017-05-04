6ec10024fa84087a53636d2790a14664f0675ec4
Benjamin <guiastrennec@gmail.com>
2017-05-04 13:46:09 +0200

    improved documentation for gg_themes

---
+++ R/gg_themes.R
@@ -0,0 +1,59 @@
+#' An additional set of themes for ggplot2
+#'
+#' @description An additional set of complete ggplot2 themes intended to make ggplot2 more readable 
+#' when used in presentation or publications. These themes also bring the \code{legend_position} option
+#' without having to call the ggplot2 theme() function to modify a complete theme.
+#' 
+#' \itemize{
+#' \item \code{theme_bw2}: Black and white theme inspired by a theme from Gunnar Yngman.
+#' \item \code{theme_readable}: Light grey theme, with dimed background and grid lines 
+#' intended to bring the focus on the data.
+#' }
+#'
+#' @param base_size Base font size.
+#' @param base_family Base font family.
+#' @param legend_position The position of legends defined as 'none', 'left', 
+#' 'right', 'bottom', 'top', or a two-element numeric vector.
+#'
+#' @examples
+#' \dontrun{
+#' ggplot(diamonds, aes(x = price, y = carat, color = cut)) +
+#'  geom_point(alpha = 0.5) +
+#'  theme_readable(legend_position = 'top')
+#' }
+#' @name gg_themes
+#' @export
+theme_bw2 <- function(base_size = 11, base_family = '', legend_position = 'right') {
+  theme_bw(base_size = base_size, base_family = base_family) %+replace% 
+    theme(plot.title = element_text(face = 'bold', size = rel(1.2), hjust = 0, 
+                                    vjust = 1, margin = margin(b = base_size/2 * 1.2)),
+          strip.text = element_text(face = 'bold', color = 'white'),
+          strip.background = element_rect(color = 'black', fill = 'black'),
+          axis.title = element_text(face = 'bold'),
+          legend.position = legend_position,
+          legend.key = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_line(color = 'grey90', size = 0.25),
+          plot.margin = grid::unit(c(0.01 ,0.01, 0.01, 0.01), 'npc'),
+          panel.background = element_rect(color = 'black', fill = 'white', size = 0.1),
+          complete = TRUE)
+}
+
+#' @rdname gg_themes
+#' @export
+theme_readable <- function(base_size = 11, base_family = '', legend_position = 'right') {
+  theme_gray(base_size = base_size, base_family = base_family) %+replace% 
+    theme(plot.title = element_text(face = 'bold', size = rel(1.2), hjust = 0, 
+                                    vjust = 1, margin = margin(b = base_size/2 * 1.2)),
+          strip.text = element_text(face = 'bold'),
+          strip.background = element_rect(color = NA, fill = 'grey88'),
+          axis.title = element_text(face = 'bold'),
+          axis.text = element_text(size = rel(0.8), colour = 'black'),
+          legend.position = legend_position,
+          legend.key = element_blank(),
+          panel.background = element_rect(color = NA, fill = 'grey95'),
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_line(color = 'grey88', size = 0.25),
+          plot.margin = grid::unit(c(0.01 ,0.01, 0.01, 0.01), 'npc'),
+          complete = TRUE)
+}
