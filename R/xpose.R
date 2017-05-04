b33aac6a2ecab30880e154f5b8eb14765c64890d
Benjamin <guiastrennec@gmail.com>
2017-05-04 11:11:44 +0200

    Added package documentation

---
+++ R/xpose.R
@@ -0,0 +1,9 @@
+#' xpose: graphical diagnostics for pharmacometric models
+#' 
+#' @description xpose was designed as an interface between pharmacometrics software outputs and modelelers. 
+#' This package aims to facilitates dataset checkout, exploration and visualization, model diagnostics, candidate covariate 
+#' identification and model comparison.
+#' 
+#' To learn more about xpose, visit our website: \url{https://guiastrennec.github.io/xpose/}.
+#' @import ggplot2
+"_PACKAGE"
