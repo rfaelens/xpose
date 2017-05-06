2ff82dcc5dbd15603fc7d918c21433eb245a6514
Benjamin <guiastrennec@gmail.com>
2017-05-06 04:32:48 +0200

    Improved consistency of read_nm_files code

--- R/read_nm_files.R
+++ R/read_nm_files.R
@@ -1,90 +1,102 @@
-#' NONMEM file parser
+#' NONMEM output file import function
 #'
-#' @description Parse NONMEM files in R format
+#' @description Quickly import NONMEM output files into R.
 #'
-#' @param file Full file name, with either .ext, .phi. .grd, .cor or .cov extension. Alternative argument to \code{dir}, \code{prefix},
-#' \code{runno} and \code{suff}.
+#' @param files Full model file names. Alternative argument to \code{dir}, \code{prefix},
+#' \code{runno} and \code{ext}.
 #' @param runno Run number to be evaluated.
-#' @param dir Location of the model file.
-#' @param prefix Prefix of the model file name.
-#' @param suff Extension of the model file.
-#' @param verbose Logical, if \code{TRUE} messages are printed to the console.
+#' @param dir Location of the model files.
+#' @param prefix Prefix of the model file names.
+#' @param ext A vector of the file extension to import. By default '.ext', '.cor', '.cov', '.phi' and '.grd' 
+#' files are listed.
+#' @param quiet Logical, if \code{FALSE} messages are printed to the console.
 #'
 #' @seealso \code{\link{xpose_data}}, \code{\link{read_nm_tables}}
-#' @return A list of \code{\link[dplyr]{tibble}} tables of class \code{} containing the following columns: 
-#' \itemize{
-##'  \item{\strong{ext}}{: The .ext file}
-##'  \item{\strong{phi}}{: The .phi file}
-##'  \item{\strong{grd}}{: The .grd file}
-##'  \item{\strong{cov}}{: The .cov file.}
-##'  \item{\strong{cor}}{: The .cor file.}
-##' }
 #' @examples
 #' \dontrun{
-#' nm_file <- read_nm_model(dir = '../models/pk/', runno = '001')
+#' out <- read_nm_files(files = 'inst/extdata/run001.ext')
 #' }
 #' @export
 
-read_nm_files <- function(file    = NULL,
-                         runno   = NULL,
-                         dir     = NULL,
-                         prefix  = 'run',
-                         suff     = c('.ext', '.cor', '.cov', '.phi', '.grd'),
-                         verbose = TRUE) {
+read_nm_files <- function(files  = NULL,
+                          runno  = NULL,
+                          dir    = NULL,
+                          prefix = 'run',
+                          ext    = c('.ext', '.cor', '.cov', '.phi', '.grd'),
+                          quiet  = FALSE) {
   
-  if (is.null(runno) && is.null(file)) {
+  if (is.null(runno) && is.null(files)) {
     stop('Argument `runno` or `file` required.', call. = FALSE)
   }
   
-  if (is.null(file)) {
-    suff  <- match.arg(suff)
-    file <- file_path(dir, paste0(prefix, runno, suff))
+  if (is.null(files)) {
+    files <- file_path(dir, paste0(prefix, runno, ext))
   }
   
-  if (!file.exists(file)) { 
-    warning('File ', basename(file), ' not found.', call. = FALSE) 
+  files <- sort(unique(files))
+  bases <- basename(files)
+  
+  if (!any(file.exists(files))) {
+    msg('No output file could be found.', quiet)
+    return()
+  } else if (any(file.exists(files))) {
+    msg(c('Reading: ', paste0(bases[file.exists(files)], collapse = ', ')), quiet)
   }
   
-  if (!file.exists(paste(dir,prefix,runno,'.ext',sep=""))) { 
-    warning('File ', paste(dir,prefix,runno,'.ext',sep=""), ' not found.', call. = FALSE) 
-    ext <- NULL
-  }else{
-    ext <- read_nm_file(paste(dir,prefix,runno,'.ext',sep=""), 'ITERATION', '.ext')
+  files %>% 
+    dplyr::tibble(path = ., name = basename(.)) %>% 
+    dplyr::filter(file.exists(.$path)) %>% 
+    purrr::by_row(~readr::read_lines(file = .$path), .to = 'raw') %>%
+    purrr::by_row(~parse_nm_files(dat = ., quiet), .collate = 'rows') %>% 
+    dplyr::select(dplyr::one_of('name', 'prob', 'subprob', 'method', 'data'))
+}  
+
+parse_nm_files <- function(dat, quiet) {
+  x <- dplyr::tibble(raw = unlist(dat$raw), prob = NA, subprob = NA, method = NA, header = FALSE)
+  tab_rows <- which(stringr::str_detect(x$raw, '^\\s*TABLE NO'))
+  
+  if (length(tab_rows) == 0) {
+    msg(c('Dropping ', dat$name, ' due to inappropriate format.'), quiet)
+    return()
   }
   
-  if (!file.exists(paste(dir,prefix,runno,'.phi',sep=""))) { 
-    warning('File ', paste(dir,prefix,runno,'.phi',sep=""), ' not found.', call. = FALSE) 
-    phi <- NULL
-  }else{
-    phi <- read_nm_file(paste(dir,prefix,runno,'.phi',sep=""), 'SUBJECT_NO', '.phi')
+  x[tab_rows, ]$prob <- stringr::str_match(x[tab_rows, ]$raw, '\\s+Problem=(\\d+)')[,2]
+  x[tab_rows, ]$subprob <- stringr::str_match(x[tab_rows, ]$raw, '\\s+Subproblem=(\\d+)')[,2]
+  x[tab_rows, ]$method  <- dplyr::case_when(stringr::str_detect(x[tab_rows, ]$raw, 'First Order Conditional') ~ 'FOCE',
+                                            stringr::str_detect(x[tab_rows, ]$raw, 'Laplace Conditional') ~ 'LCE', 
+                                            stringr::str_detect(x[tab_rows, ]$raw, 'Iterative Two Stage') ~ 'ITS',
+                                            stringr::str_detect(x[tab_rows, ]$raw, 'Importance Sampling') ~ 'IMP',
+                                            stringr::str_detect(x[tab_rows, ]$raw, 'Stochastic Approximation') ~ 'SAEM',
+                                            TRUE ~ 'BAYES')
+  
+  # Assumes that header are always present
+  x[tab_rows + 1, ]$header <- TRUE
+  
+  # Guess column separator from the first problem only
+  sep <- dplyr::case_when(
+    stringr::str_detect(x[tab_rows[1] + 1, ]$raw, ';[A-z]+') ~ ';[A-z]|[A-z];',
+    stringr::str_detect(x[tab_rows[1] + 1, ]$raw, ',[A-z]+') ~ ',[A-z]|[A-z],',
+    TRUE ~ '\\s+')
+  
+  x %>% 
+    tidyr::fill(dplyr::one_of('prob', 'subprob', 'method')) %>% 
+    dplyr::slice(-tab_rows) %>%
+    dplyr::mutate(raw = stringr::str_trim(.$raw, side = 'both')) %>% 
+    purrr::slice_rows(c('prob', 'subprob', 'method')) %>% 
+    purrr::by_slice(raw_to_tibble, sep, quiet, file = dat$name, .to = 'data')
+}  
+
+raw_to_tibble <- function(x, sep, quiet, file) {
+  header <- x$raw[x$header] %>% 
+    stringr::str_split(pattern = sep) %>% 
+    purrr::flatten_chr()
+  
+  if (any(is.na(header))) {
+    msg(c('Issue encountered while parsing ', file), quiet)
+    return()
   }
   
-  if (!file.exists(paste(dir,prefix,runno,'.grd',sep=""))) { 
-    warning('File ', paste(dir,prefix,runno,'.grd',sep=""), ' not found.', call. = FALSE) 
-    grd <- NULL
-  }else{
-    grd <- read_nm_file(paste(dir,prefix,runno,'.grd',sep=""), 'ITERATION', '.ext')
-  }
-  
-  if (!file.exists(paste(dir,prefix,runno,'.cov',sep=""))) { 
-    warning('File ',paste(dir,prefix,runno,'.cov',sep=""), ' not found.', call. = FALSE) 
-    cov1 <- NULL
-  }else{
-    cov1 <- read_nm_file(paste(dir,prefix,runno,'.cov',sep=""), 'NAME', '.cor')
-  }
-  
-  if (!file.exists(paste(dir,prefix,runno,'.cor',sep=""))) { 
-    warning('File ', paste(dir,prefix,runno,'.cor',sep=""), ' not found.', call. = FALSE) 
-    cor1 <- NULL
-  }else{
-    cor1 <- read_nm_file(paste(dir,prefix,runno,'.cor',sep=""), 'NAME', '.cov')
-  }
-  
-  
-  # As list
-  nm_files <- list(ext= ext, phi= phi,grd= grd, cov=cov1,cor=cor1) 
-  
-  
-  nm_files
-  
+  x[!x$header, ] %>%   
+    tidyr::separate(col  = 'raw', sep  = sep, into = header) %>% 
+    dplyr::select(-ncol(.))
 }
