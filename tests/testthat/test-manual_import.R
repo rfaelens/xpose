a5f584574543edaf37018c8f630dca2b50b590e1
Benjamin <guiastrennec@gmail.com>
2017-05-19 15:20:43 +0200

    added new tests

---
+++ tests/testthat/test-manual_import.R
@@ -0,0 +1,18 @@
+context('Check manual_import')
+
+ctrl_str <- list(tab_suffix = '',
+                 sim_suffix = 'sim',
+                 tab_names = c('sdtab', 'mutab', 'patab', 'catab', 'cotab', 'mytab', 'extra', 'xptab', 'cwtab'))
+
+ctrl_data <- xpose_data(file = 'run001.lst', quiet = TRUE)$data$index[[1]]
+ctrl_data <- dplyr::arrange_(.data = ctrl_data, .dots = 'tables')
+
+# Tests start here --------------------------------------------------------
+test_that('manual_nm_import function works properly', {
+  expect_equal(manual_nm_import(), ctrl_str)
+})
+
+test_that('list_nm_tables_manual function works properly', {
+  test <- xpose_data(file = 'run001.lst', manual_import = manual_nm_import(), quiet = FALSE)$data$index[[1]]
+  expect_identical(test, ctrl_data)
+})
