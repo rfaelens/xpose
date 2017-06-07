876ff8373e7a21aea1dd30c225bae9b0cc0254b3
Benjamin <guiastrennec@gmail.com>
2017-06-07 17:43:19 +0200

    Added new tests

--- tests/testthat/test-basic-gof.R
+++ tests/testthat/test-basic-gof.R
@@ -2,22 +2,31 @@ context('Check basic gof functions')
 
 # Tests start here --------------------------------------------------------
 
-test_that("errors are returned for missing xpdb", {
+test_that("errors are returned for missing xpdb_ex_pk", {
   expect_error(dv_vs_pred())
   expect_error(dv_vs_ipred())
   expect_error(dv_vs_cpred())
+  expect_error(dv_vs_idv())
+  expect_error(ipred_vs_idv())
+  expect_error(pred_vs_idv())
+  expect_error(dv_preds_vs_idv())
   expect_error(res_vs_idv())
   expect_error(res_vs_pred())
   expect_error(absval_res_vs_idv())
   expect_error(absval_res_vs_pred())
 })
 
-test_that("xpose plot objects are returned with appropriate xpdb", {
-  expect_equal(class(dv_vs_pred(xpdb_ex_pk)), c('xpose_plot', 'gg', 'ggplot'))
-  expect_equal(class(dv_vs_ipred(xpdb_ex_pk)), c('xpose_plot', 'gg', 'ggplot'))
-  expect_equal(class(dv_vs_cpred(xpdb_ex_pk)), c('xpose_plot', 'gg', 'ggplot'))
-  expect_equal(class(res_vs_idv(xpdb_ex_pk)), c('xpose_plot', 'gg', 'ggplot'))
-  expect_equal(class(res_vs_pred(xpdb_ex_pk)), c('xpose_plot', 'gg', 'ggplot'))
-  expect_equal(class(absval_res_vs_idv(xpdb_ex_pk)), c('xpose_plot', 'gg', 'ggplot'))
-  expect_equal(class(absval_res_vs_pred(xpdb_ex_pk)), c('xpose_plot', 'gg', 'ggplot'))
+test_that("xpose plot objects are returned with appropriate xpdb_ex_pk", {
+  expect_true(is.xpose.plot(dv_vs_pred(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(dv_vs_ipred(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(dv_vs_cpred(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(dv_vs_idv(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(ipred_vs_idv(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(pred_vs_idv(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(dv_preds_vs_idv(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(res_vs_idv(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(res_vs_pred(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(absval_res_vs_idv(xpdb_ex_pk)))
+  expect_true(is.xpose.plot(absval_res_vs_pred(xpdb_ex_pk)))
 })
+
