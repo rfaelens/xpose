e5047d189d35f8d90cfa1341bf2dd26eeb2b7e9d
Benjamin <guiastrennec@gmail.com>
2017-06-08 16:04:59 +0200

    grouped res plots documentation

--- R/plot_resiudals.R
+++ R/plot_resiudals.R
@@ -17,7 +17,11 @@
 #' @inheritSection xplot_scatter Template titles
 #' @seealso \code{\link{xplot_scatter}}
 #' @examples
+#' # Standard residual
 #' res_vs_pred(xpdb_ex_pk, res = 'CWRES')
+#' 
+#' # Absolute value of the residuals
+#' absval_res_vs_pred(xpdb_ex_pk, res = 'CWRES')
 #' 
 #' @export
 res_vs_pred <- function(xpdb,
@@ -47,6 +51,37 @@ res_vs_pred <- function(xpdb,
                 guides_slope = 0, ...)
 }
 
+
+#' @rdname res_vs_pred
+#' @export
+absval_res_vs_pred <- function(xpdb,
+                               mapping  = NULL,
+                               res      = 'CWRES',
+                               group    = 'ID',
+                               type     = 'pls',
+                               facets   = NULL,
+                               title    = '@y vs. @x | @run',
+                               subtitle = 'Ofv: @ofv',
+                               caption  = '@dir',
+                               log      = NULL,
+                               guides   = FALSE,
+                               problem,
+                               ...) {
+  if (missing(problem)) problem <- last_data_problem(xpdb, simtab = FALSE)
+  xplot_scatter(xpdb = xpdb, group = group,
+                data_opt = data_opt_set(problem = problem, 
+                                        filter = only_obs(xpdb, problem)),
+                mapping = aes_c(aes_string(x = xp_var(xpdb, problem, type = 'pred')$col, 
+                                           y = stringr::str_c('abs(', toupper(res), ')')), mapping),
+                type = type, guides = guides, panel_facets = facets, 
+                xscale = check_scales('x', log), 
+                yscale = check_scales('y', log), 
+                title = title, subtitle = subtitle, caption = caption,
+                plot_name = as.character(match.call()[[1]]),
+                guides_slope = 0, ...)
+}
+
+
 #' Residuals plotted against the independent variable
 #'
 #' @description Model residuals plotted against the independent variable (IDV).
@@ -66,7 +101,11 @@ res_vs_pred <- function(xpdb,
 #' @inheritSection xplot_scatter Template titles
 #' @seealso \code{\link{xplot_scatter}}
 #' @examples
+#' # Standard residual
 #' res_vs_idv(xpdb_ex_pk, res = 'CWRES', aes(x = TAD))
+#' 
+#' # Absolute value of the residuals
+#' absval_res_vs_idv(xpdb_ex_pk, res = 'CWRES', aes(x = TAD))
 #' 
 #' @export
 res_vs_idv <- function(xpdb,
@@ -96,78 +135,8 @@ res_vs_idv <- function(xpdb,
                 guides_slope = 0, ...)
 }
 
-#' Absolute value of residuals plotted against population predictions
-#'
-#' @description Absolute value of the model residuals plotted against the population predictions (PRED).
-#' 
-#' The residuals can be one of:
-#' \itemize{
-#'   \item RES: model residuals
-#'   \item WRES: weighted model residuals
-#'   \item CWRES: conditional weighted model residuals
-#'   \item EWRES/ECWRES: Monte Carlo based model residuals
-#'   \item NPDE: Normalized prediction distribution error 
-#' }
-#'
-#' @inheritParams dv_vs_pred
-#' @param res Type of residual to be used. Default is "CWRES".
-#' 
-#' @inheritSection xplot_scatter Layers mapping
-#' @inheritSection xplot_scatter Template titles
-#' @seealso \code{\link{xplot_scatter}}
-#' @examples
-#' absval_res_vs_pred(xpdb_ex_pk, res = 'CWRES')
-#' 
-#' @export
-absval_res_vs_pred <- function(xpdb,
-                               mapping  = NULL,
-                               res      = 'CWRES',
-                               group    = 'ID',
-                               type     = 'pls',
-                               facets   = NULL,
-                               title    = '@y vs. @x | @run',
-                               subtitle = 'Ofv: @ofv',
-                               caption  = '@dir',
-                               log      = NULL,
-                               guides   = FALSE,
-                               problem,
-                               ...) {
-  if (missing(problem)) problem <- last_data_problem(xpdb, simtab = FALSE)
-  xplot_scatter(xpdb = xpdb, group = group,
-                data_opt = data_opt_set(problem = problem, 
-                                    filter = only_obs(xpdb, problem)),
-                mapping = aes_c(aes_string(x = xp_var(xpdb, problem, type = 'pred')$col, 
-                                           y = stringr::str_c('abs(', toupper(res), ')')), mapping),
-                type = type, guides = guides, panel_facets = facets, 
-                xscale = check_scales('x', log), 
-                yscale = check_scales('y', log), 
-                title = title, subtitle = subtitle, caption = caption,
-                plot_name = as.character(match.call()[[1]]),
-                guides_slope = 0, ...)
-}
 
-#' Absolute value of residuals plotted against independent variable
-#'
-#' @description Absolute value of the model residuals plotted against the independent variable (IDV).
-#' 
-#' The residuals can be one of:
-#' \itemize{
-#'   \item RES: model residuals
-#'   \item WRES: weighted model residuals
-#'   \item CWRES: conditional weighted model residuals
-#'   \item EWRES/ECWRES: Monte Carlo based model residuals
-#'   \item NPDE: Normalized prediction distribution error 
-#' }
-#'
-#' @inheritParams dv_vs_pred
-#' @param res Type of residual to be used. Default is "CWRES".
-#' 
-#' @inheritSection xplot_scatter Layers mapping
-#' @inheritSection xplot_scatter Template titles
-#' @seealso \code{\link{xplot_scatter}}
-#' @examples
-#' absval_res_vs_idv(xpdb_ex_pk, res = 'CWRES', aes(x = TAD))
-#' 
+#' @rdname res_vs_idv
 #' @export
 absval_res_vs_idv <- function(xpdb,
                               mapping  = NULL,
@@ -185,7 +154,7 @@ absval_res_vs_idv <- function(xpdb,
   if (missing(problem)) problem <- last_data_problem(xpdb, simtab = FALSE)
   xplot_scatter(xpdb = xpdb, group = group,
                 data_opt = data_opt_set(problem = problem, 
-                                    filter = only_obs(xpdb, problem)),
+                                        filter = only_obs(xpdb, problem)),
                 mapping = aes_c(aes_string(x = xp_var(xpdb, problem, type = 'idv')$col, 
                                            y = stringr::str_c('abs(', toupper(res), ')')), mapping), 
                 type = type, guides = guides, panel_facets = facets, 
