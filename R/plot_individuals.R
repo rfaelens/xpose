cca3d968c902f27182a5d22841918ece3ae5cf3a
Benjamin <guiastrennec@gmail.com>
2017-06-12 21:46:28 +0200

    added individual plots

---
+++ R/plot_individuals.R
@@ -0,0 +1,45 @@
+#' Observations, individual predictions and population predictions plotted against 
+#' the independent variable for every individual
+#'
+#' @description Observations (DV), individual predictions (IPRED) and population predictions (PRED) plotted against 
+#' the independent variable for every individual
+#' 
+#' @inheritParams dv_vs_pred
+#' @inheritSection xplot_scatter Layers mapping
+#' @inheritSection xplot_scatter Template titles
+#' @seealso \code{\link{xplot_scatter}}
+#' @examples
+#' ind_plot(xpdb_ex_pk, aes(x = TAD))
+#' 
+#' @export
+ind_plots <- function(xpdb,
+                      mapping  = NULL,
+                      group    = 'variable',
+                      type     = 'l',
+                      facets   = NULL,
+                      title    = 'Individual plots vs. @x | @run',
+                      subtitle = 'Ofv: @ofv, Eps shrink: @epsshk',
+                      caption  = '@dir',
+                      log      = NULL,
+                      problem,
+                      ...) {
+  if (missing(problem)) problem <- last_data_problem(xpdb, simtab = FALSE)
+  if (missing(facets)) facets <- xp_var(xpdb, problem, type = 'id')$col
+  
+  xplot_scatter(xpdb = xpdb, group = group,
+                data_opt = data_opt_set(problem = problem, 
+                                        source = 'data',
+                                        filter = only_obs(xpdb, problem),
+                                        tidy   = TRUE,
+                                        value_col = xp_var(xpdb, problem, 
+                                                           type = c('dv', 'pred', 'ipred'))$col),
+                mapping = aes_c(aes_string(x = xp_var(xpdb, problem, type = 'idv')$col, 
+                                           y = 'value', line_color = 'variable',
+                                           point_color = 'variable'), mapping),
+                type = type, panel_facets = facets,
+                xscale = check_scales('x', log),
+                yscale = check_scales('y', log), 
+                title = title, subtitle = subtitle, caption = caption,
+                plot_name = as.character(match.call()[[1]]),
+                panel_scales = 'free_y', ...)
+}
