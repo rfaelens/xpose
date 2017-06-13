a219ddaede81ede006cdf2c7226b6898e6c044d5
Benjamin <guiastrennec@gmail.com>
2017-06-13 16:39:47 +0200

    started adding vpc functionality

---
+++ R/plot_vpc.R
@@ -0,0 +1,139 @@
+#' Visual predictive checks
+#'
+#' @description Generate visual predictive checks (VPC)
+#' 
+#' @param xpdb An xpose database object.
+#' @param vpc_opt A list of options regarding binning, pi and ci computation. 
+#' For more information see \code{\link{vpc_opt_set}}.
+#' @param mapping List of aesthetics mappings to be used for the xpose plot 
+#' (e.g. \code{point_color}).
+#' @param type String setting the type of plot to be used points 'p',
+#' line 'l', area 'a' and text 't' or any combination of the four.
+#' @param smooth Should the bins be smoothed (connect bin midpoints, default) or shown as rectangular boxes.
+#' @param facets Either a character string to use \link[ggplot2]{facet_wrap} 
+#' or a formula to use \link[ggplot2]{facet_grid}.
+#' @param title Plot title. Use \code{NULL} to remove.
+#' @param subtitle Plot subtitle. Use \code{NULL} to remove.
+#' @param caption Page caption. Use \code{NULL} to remove.
+#' @param log String assigning logarithmic scale to axes, can be either '', 
+#' 'x', y' or 'xy'.       
+#' @param psn_folder Specify a PsN-generated VPC-folder.
+#' @param obs_problem Alternative to the option `psn_folder`. The $problem number to be used for observations.
+#' By default returns the last estimation problem.
+#' @param sim_problem Alternative to the option `psn_folder`. The $problem number to be used for simulations.
+#' By default returns the last simulation problem.
+#' @param quiet Logical, if \code{FALSE} messages are printed to the console.
+#' @param ... any additional aesthetics.
+
+#' @inheritSection xplot_scatter Layers mapping
+#' @inheritSection xplot_scatter Template titles
+#' @seealso \code{\link[vpc]{vpc}}
+#' @examples
+#' \dontrun{
+#' # Simple vpc using simulated data from the xpdb
+#' vpc(xpdb_ex_pk)
+#' }
+#' @export
+vpc <- function(xpdb,
+                vpc_opt    = NULL, 
+                mapping    = NULL,
+                #group     = 'variable',
+                type       = 'alp',
+                smooth     = TRUE,
+                facets     = NULL,
+                title      = 'Visual predictive checks | @run',
+                subtitle   = 'Nsim: @nsim, PI: @vpcpi',
+                caption    = '@dir',
+                log        = NULL,
+                psn_folder = NULL,
+                obs_problem,
+                sim_problem,
+                quiet,
+                ...) {
+  
+  # Check input
+  if (!is.xpdb(xpdb)) { 
+    msg('Bad input to the argument`xpdb`', ifelse(missing(quiet), TRUE, quiet))
+    return()
+  }
+  
+  if (missing(quiet)) quiet <- xpdb$options$quiet
+  
+  if (is.null(psn_folder)) {
+    if (missing(obs_problem)) obs_problem <- last_data_problem(xpdb, simtab = FALSE)
+    if (missing(sim_problem)) sim_problem <- last_data_problem(xpdb, simtab = TRUE)
+    if (is.na(sim_problem) || is.na(obs_problem)) {
+      msg('Both simulation and estimation problems are required. Alternatively a `psn_folder` can be specified.', quiet)
+      return()
+    }
+    msg(c('Using prob no.', sim_problem, ' for simulations and prob no.', obs_problem, ' for observations.'), quiet) 
+    obs_data   <- get_data(xpdb, problem = obs_problem) 
+    sim_data   <- get_data(xpdb, problem = sim_problem)
+  } else {
+    msg(c('Using psn data from: ', psn_folder), quiet) 
+    obs_data <- NULL
+    sim_data <- NULL
+  } 
+  
+  if (is.null(vpc_opt)) vpc_opt <- vpc_opt_set()
+  
+  if (is.formula(facets)) {
+    stratify <- all.vars(facets)
+  } else {
+    stratify <- facets
+  }
+  
+  # Generate vpc data
+  vpc_dat <- vpc::vpc(obs = obs_data, sim = sim_data, psn_folder = psn_folder, bins = vpc_opt$bins, 
+                      n_bins = vpc_opt$n_bins, bin_mid = vpc_opt$bin_mid, obs_cols = vpc_opt$obs_cols, 
+                      sim_cols = vpc_opt$sim_cols, stratify = stratify, pred_corr = vpc_opt$pred_corr, 
+                      pred_corr_lower_bnd = vpc_opt$pred_corr_lower_bnd, pi = vpc_opt$pi, ci = vpc_opt$ci, 
+                      uloq = vpc_opt$uloq, lloq = vpc_opt$lloq, smooth = smooth, vpcdb = TRUE, verbose = !quiet) 
+  vpc_dat  
+}
+
+
+#' Create options for data import
+#' 
+#' @description Provide a list of options to the general plotting functions such as 
+#' \code{xplot_scatter} in order to create appropriate data input for ggplot2.
+#' 
+#' @param bins Binning method, can be one of 'density', 'time', 'data', 'none', or one of the approaches 
+#' available in \code{classInterval()} such as 'jenks' (default), 'pretty', or a numeric vector specifying 
+#' the bin separators.
+#' @param n_bins When using the 'auto' binning method, what number of bins to aim for.
+#' @param bin_mid Specify how to is the mid bin value calculated, can be either 'mean' for the mean of all 
+#' timepoints (default) or 'middle' to use the average of the bin boundaries.
+#' @param obs_cols Observation dataset column names (list elements: 'dv', 'idv', 'id', 'pred')
+#' @param sim_cols Simulation dataset column names (list elements: 'dv', 'idv', 'id', 'pred')
+#' @param pred_corr Logical, should a prediction correction (pcVPC) of the data be used.
+#' @param pred_corr_lower_bnd Lower bound for the prediction-correction.
+#' @param ci Confidence interval around the percentiles to plot. Default is (0.05, 0.95)
+#' @param pi Simulated prediction interval to plot. Default is c(0.05, 0.95).
+#' @param lloq Number or NULL indicating lower limit of quantification. Default is NULL.               
+#' @param uloq Number or NULL indicating upper limit of quantification. Default is NULL.
+#'
+#' @seealso \code{{vpc}}
+#' 
+#' @examples
+#' vpc_opt_set()
+#' 
+#' @export
+vpc_opt_set <- function(bins                = 'jenks',
+                        n_bins              = 'auto',
+                        bin_mid             = 'mean',
+                        obs_cols            = NULL,
+                        sim_cols            = NULL,
+                        pred_corr           = FALSE, 
+                        pred_corr_lower_bnd = 0,
+                        ci                  = c(0.05, 0.95), 
+                        pi                  = c(0.05, 0.95), 
+                        lloq                = NULL, 
+                        uloq                = NULL) {
+  
+  list(bins = bins, n_bins = n_bins, bin_mid = bin_mid,
+       obs_cols = obs_cols, sim_cols = sim_cols, # Temporary?
+       pred_corr = pred_corr, 
+       pred_corr_lower_bnd = pred_corr_lower_bnd,
+       pi = pi, ci = ci, lloq = lloq, uloq = uloq)
+}
---
+++ R/vpc_data.R
@@ -0,0 +1,333 @@
+#' Import and compute data for visual predictive plots
+#'
+#' @description Function designed to import and compute data for VPC. This function is a modified version of the vpc::vpc() function
+#' developped by Ron Keizer \url{https://github.com/ronkeizer/vpc}.
+#' 
+#' @param xpdb An xpose database object.
+#' @param psn_folder Optional, specify a PsN-generated VPC-folder.
+#' @param bins Binning method, can be one of 'density', 'time', 'data', 'none', or one of the approaches 
+#' available in \code{classInterval()} such as 'jenks' (default), 'pretty', or a numeric vector specifying 
+#' the bin separators.
+#' @param n_bins When using the 'auto' binning method, what number of bins to aim for.
+#' @param bin_mid Specify how to is the mid bin value calculated, can be either 'mean' for the mean of all 
+#' timepoints (default) or 'middle' to use the average of the bin boundaries.
+#' @param obs_cols Observation dataset column names (list elements: 'dv', 'idv', 'id', 'pred')
+#' @param sim_cols Simulation dataset column names (list elements: 'dv', 'idv', 'id', 'pred')
+#' @param stratify Character vector of stratification variables. Only 1 or 2 stratification variables can be supplied.
+#' @param pred_corr Logical, should a prediction correction (pcVPC) of the data be used.
+#' @param pred_corr_lower_bnd Lower bound for the prediction-correction.
+#' @param ci Confidence interval around the percentiles to plot. Default is (0.05, 0.95)
+#' @param pi Simulated prediction interval to plot. Default is c(0.05, 0.95).
+#' @param uloq Number or NULL indicating upper limit of quantification. Default is NULL.
+#' @param lloq Number or NULL indicating lower limit of quantification. Default is NULL.
+#' @param smooth Should the bins be smoothed (connect bin midpoints, default) or shown as rectangular boxes.
+#' @param obs_problem Alternative to the option `psn_folder`. The $problem number to be used for observations.
+#' By default returns the last estimation problem.
+#' @param sim_problem Alternative to the option `psn_folder`. The $problem number to be used for simulations.
+#' By default returns the last simulation problem.
+#' @param obs_problem Alternative to the option `psn_folder`. The $problem number to be used for observations.
+#' By default returns the last estimation problem.
+#' @param sim_problem Alternative to the option `psn_folder`. The $problem number to be used for simulations.
+#' By default returns the last simulation problem.
+#' @param quiet Logical, if \code{FALSE} messages are printed to the console.
+#' @param ... any additional aesthetics.
+#' 
+#' @export
+vpc_data <- function(xpdb       = NULL,
+                     psn_folder = NULL,
+                     bins       = 'jenks',
+                     n_bins     = 'auto',
+                     bin_mid    = 'mean',
+                     obs_cols   = NULL,
+                     sim_cols   = NULL,
+                     stratify   = NULL,
+                     pred_corr  = FALSE,
+                     pred_corr_lower_bnd = 0,
+                     pi         = c(0.05, 0.95),
+                     ci         = c(0.05, 0.95),
+                     uloq       = NULL,
+                     lloq       = NULL,
+                     smooth     = TRUE,
+                     obs_problem,
+                     sim_problem,
+                     quiet,
+                     ...) {
+  
+  # Check input
+  if (!is.xpdb(xpdb)) { 
+    msg('Bad input to the argument`xpdb`', ifelse(missing(quiet), TRUE, quiet))
+    return()
+  }
+  
+  if (missing(quiet)) quiet <- xpdb$options$quiet
+  
+  if (is.null(psn_folder)) {
+    if (missing(obs_problem)) obs_problem <- last_data_problem(xpdb, simtab = FALSE)
+    if (missing(sim_problem)) sim_problem <- last_data_problem(xpdb, simtab = TRUE)
+    
+    if (is.na(sim_problem) & is.na(obs_problem)) {
+      msg('At least a simulation or an observation dataset are required to create a plot! Alternatively a `psn_folder` can be specified.', quiet)
+      return()
+    }
+    msg(c('Using prob no.', sim_problem, ' for simulations and prob no.', obs_problem, ' for observations.'), quiet) 
+    obs_data   <- get_data(xpdb, problem = obs_problem)
+    sim_data   <- get_data(xpdb, problem = sim_problem)
+    
+  } else {
+    msg(c('Using psn data from: ', psn_folder), quiet) 
+    obs_data <- read_nm_tables(files = file_path(psn_folder, 'm1', dir(paste0(psn_folder, '/m1'), pattern = 'original.npctab')[1]))
+    sim_data <- read_nm_tables(files = file_path(psn_folder, 'm1', dir(paste0(psn_folder, '/m1'), pattern='simulation.1.npctab')[1]))
+  } 
+  
+  if (is.null(obs) & is.null(sim)) {
+    stop('At least a simulation or an observation dataset are required to create a plot!')
+  }
+  if (verbose) {
+    message('Configuring and initializing...')
+  }
+  if (!is.null(obs)) {
+    software_type <- guess_software(software, obs)
+  } else {
+    software_type <- guess_software(software, sim)
+  }
+  
+  ## software specific parsing, if necessary
+  if (software_type == 'PKPDsim') {
+    if (!is.null(obs)) {
+      if ('obs' %in% obs$comp) {
+        obs <- obs %>% dplyr::filter(comp == 'obs')
+      }
+      obs <- data.frame(obs)
+    }
+    if (!is.null(sim)) {
+      if ('obs' %in% sim$comp) {
+        sim <- sim %>% dplyr::filter(comp == 'obs')
+      }
+      sim <- data.frame(sim)
+    }
+  }
+  
+  ## define what to show in plot
+  show <- replace_list_elements(show_default, show)
+  
+  ## define column names
+  cols <- define_data_columns(sim, obs, sim_cols, obs_cols, software_type)
+  if (!is.null(obs)) {
+    old_class <- class(obs)
+    class(obs) <- c(software_type, old_class)
+  }
+  if (!is.null(sim)) {
+    old_class <- class(sim)
+    class(sim) <- c(software_type, old_class)
+  }
+  
+  ## checking whether stratification columns are available
+  if (!is.null(stratify)) {
+    if (verbose) {
+      message('Stratifying oberved data...')
+    }
+    if (!is.null(obs)) {
+      check_stratification_columns_available(obs, stratify, 'observation')
+    }
+    if (!is.null(sim)) {
+      check_stratification_columns_available(sim, stratify, 'simulation')
+    }
+  }
+  if (!is.null(stratify_color)) {
+    if (verbose) {
+      message('Stratifying simulated data...')
+    }
+    if (!is.null(obs)) {
+      check_stratification_columns_available(obs, stratify_color, 'observation')
+    }
+    if (!is.null(sim)) {
+      check_stratification_columns_available(sim, stratify_color, 'simulation')
+    }
+  }
+  
+  ## parse data into specific format
+  if (!is.null(obs)) {
+    if (verbose) {
+      message('Parsing observed data...')
+    }
+    obs <- filter_dv(obs, verbose)
+    obs <- format_vpc_input_data(obs, cols$obs, lloq, uloq, stratify, bins, log_y, log_y_min, 'observed', verbose)
+  }
+  if (!is.null(sim)) {
+    if (verbose) {
+      message('Parsing simulated data...')
+    }
+    sim <- filter_dv(sim, verbose)
+    sim <- format_vpc_input_data(sim, cols$sim, lloq, uloq, stratify, bins, log_y, log_y_min, 'simulated', verbose)
+  }
+  
+  stratify_original <- stratify
+  if (!is.null(stratify_color)) {
+    if (is.null(stratify)) {
+      stratify <- stratify_color
+    }
+    if (length(stratify_color) > 1) {
+      stop('Error: please specify only 1 stratification variable for color!')
+    }
+    if (!stratify_color %in% stratify) {
+      stratify_original <- stratify
+      stratify <- c(stratify, stratify_color)
+    }
+  }
+  if (class(bins) != 'numeric') {
+    if(!is.null(obs)) {
+      bins <- auto_bin(obs, bins, n_bins)
+    } else { # get from sim
+      bins <- auto_bin(sim, bins, n_bins)
+    }
+    if (is.null(bins)) {
+      msg('Automatic binning unsuccessful, try increasing the number of bins, or specify vector of bin separators manually.', verbose)
+    }
+  }
+  bins <- unique(bins)
+  if (verbose) {
+    message(paste0('Binning: ', paste(bins, collapse=' ')))
+  }
+  if (!is.null(obs)) {
+    obs <- bin_data(obs, bins, 'idv')
+  }
+  if (!is.null(sim)) {
+    sim <- bin_data(sim, bins, 'idv')
+  }
+  
+  if (pred_corr) {
+    if (!is.null(obs) & !cols$obs$pred %in% names(obs)) {
+      msg('Warning: Prediction-correction: specified pred-variable not found in observation dataset, trying to get from simulated dataset...', verbose)
+      if (!cols$obs$pred %in% names(sim)) {
+        stop('Error: Prediction-correction: specified pred-variable not found in simulated dataset, not able to perform pred-correction!')
+      } else {
+        obs <- obs %>% dplyr::ungroup()
+        obs[[cols$obs$pred]] <- unlist(sim[1:length(obs$id), cols$sim$pred])
+        msg ('OK', verbose)
+      }
+    } else {
+      if (!cols$sim$pred %in% names(sim)) {
+        stop('Warning: Prediction-correction: specified pred-variable not found in simulated dataset, not able to perform pred-correction!')
+      }
+    }
+    if (!is.null(obs)) {
+      obs$pred = obs[[cols$obs$pred]]
+    }
+    if (!is.null(sim)) {
+      sim$pred <- sim[[cols$sim$pred]]
+    }
+  }
+  if (!is.null(obs)) {
+    if (pred_corr) {
+      if (verbose) {
+        message('Performing prediction-correction on observed data...')
+      }
+      obs <- obs %>% dplyr::group_by(strat, bin) %>% dplyr::mutate(pred_bin = median(as.num(pred)))
+      obs[obs$pred != 0,]$dv <- pred_corr_lower_bnd + (obs[obs$pred != 0,]$dv - pred_corr_lower_bnd) * (obs[obs$pred != 0,]$pred_bin - pred_corr_lower_bnd) / (obs[obs$pred != 0,]$pred - pred_corr_lower_bnd)
+    }
+  }
+  if (!is.null(sim)) {
+    sim$sim <- add_sim_index_number(sim, id = 'id')
+    if (pred_corr) {
+      if (verbose) {
+        message('Performing prediction-correction on simulated data...')
+      }
+      sim <- sim %>% dplyr::group_by(strat, bin) %>% dplyr::mutate(pred_bin = median(pred))
+      sim[sim$pred != 0,]$dv <- pred_corr_lower_bnd + (sim[sim$pred != 0,]$dv - pred_corr_lower_bnd) * (sim[sim$pred != 0,]$pred_bin - pred_corr_lower_bnd) / (sim[sim$pred != 0,]$pred - pred_corr_lower_bnd)
+    }
+  }
+  if (!is.null(sim)) {
+    if (verbose) {
+      message('Calculating statistics for simulated data...')
+    }
+    tmp1 <- sim %>% dplyr::group_by(strat, sim, bin)
+    aggr_sim <- data.frame(cbind(tmp1 %>% dplyr::summarise(quantile(dv, pi[1])),
+                                 tmp1 %>% dplyr::summarise(quantile(dv, 0.5 )),
+                                 tmp1 %>% dplyr::summarise(quantile(dv, pi[2])),
+                                 tmp1 %>% dplyr::summarise(mean(idv))))
+    aggr_sim <- aggr_sim[,-grep('(bin.|strat.|sim.)', colnames(aggr_sim))]
+    colnames(aggr_sim)[grep('quantile', colnames(aggr_sim))] <- c('q5', 'q50', 'q95')
+    colnames(aggr_sim)[length(aggr_sim[1,])] <- 'mn_idv'
+    tmp <- aggr_sim %>% dplyr::group_by(strat, bin)
+    vpc_dat <- data.frame(cbind(tmp %>% dplyr::summarise(quantile(q5, ci[1])),
+                                tmp %>% dplyr::summarise(quantile(q5, 0.5)),
+                                tmp %>% dplyr::summarise(quantile(q5, ci[2])),
+                                tmp %>% dplyr::summarise(quantile(q50, ci[1])),
+                                tmp %>% dplyr::summarise(quantile(q50, 0.5)),
+                                tmp %>% dplyr::summarise(quantile(q50, ci[2])),
+                                tmp %>% dplyr::summarise(quantile(q95, ci[1])),
+                                tmp %>% dplyr::summarise(quantile(q95, 0.5)),
+                                tmp %>% dplyr::summarise(quantile(q95, ci[2])),
+                                tmp %>% dplyr::summarise(mean(mn_idv))))
+    vpc_dat <- vpc_dat[,-grep('(bin.|strat.)', colnames(vpc_dat))]
+    colnames(vpc_dat) <- c('strat', 'bin',
+                           'q5.low','q5.med','q5.up',
+                           'q50.low','q50.med','q50.up',
+                           'q95.low','q95.med','q95.up',
+                           'bin_mid')
+    vpc_dat$bin_min <- rep(bins[1:(length(bins)-1)], length(unique(vpc_dat$strat)))[vpc_dat$bin]
+    vpc_dat$bin_max <- rep(bins[2:length(bins)], length(unique(vpc_dat$strat)))[vpc_dat$bin]
+    if (bin_mid == 'middle') {
+      vpc_dat$bin_mid <- apply(cbind(vpc_dat$bin_min, vpc_dat$bin_max), 1, mean)
+    }
+  } else {
+    vpc_dat <- NULL
+  }
+  if (!is.null(obs)) {
+    if (verbose) {
+      message('Calculating statistics for observed data...')
+    }
+    tmp1 <- obs %>% dplyr::group_by(strat,bin)
+    aggr_obs <- data.frame(cbind(tmp1 %>% dplyr::summarise(quantile(dv, pi[1])),
+                                 tmp1 %>% dplyr::summarise(quantile(dv, 0.5 )),
+                                 tmp1 %>% dplyr::summarise(quantile(dv, pi[2])),
+                                 tmp1 %>% dplyr::summarise(mean(idv))))
+    aggr_obs <- aggr_obs[,-grep('(bin.|strat.|sim.)', colnames(aggr_obs))]
+    colnames(aggr_obs) <- c('strat', 'bin', 'obs5','obs50','obs95', 'bin_mid')
+    aggr_obs$bin_min <- rep(bins[1:(length(bins)-1)], length(unique(aggr_obs$strat)) )[aggr_obs$bin]
+    aggr_obs$bin_max <- rep(bins[2:length(bins)], length(unique(aggr_obs$strat)) )[aggr_obs$bin]
+    if (bin_mid == 'middle') {
+      aggr_obs$bin_mid <- apply(cbind(aggr_obs$bin_min, aggr_obs$bin_max), 1, mean)
+    }
+  } else {
+    aggr_obs <- NULL
+  }
+  if (is.null(xlab)) {
+    xlab <- cols$obs$idv
+  }
+  if (is.null(ylab)) {
+    ylab <- cols$obs$dv
+  }
+  if (!is.null(stratify_original)) {
+    if (length(stratify) == 2) {
+      vpc_dat$strat1 <- unlist(strsplit(as.character(vpc_dat$strat), ', '))[(1:length(vpc_dat$strat)*2) - 1]
+      vpc_dat$strat2 <- unlist(strsplit(as.character(vpc_dat$strat), ', '))[(1:length(vpc_dat$strat)*2)]
+      aggr_obs$strat1 <- unlist(strsplit(as.character(aggr_obs$strat), ', '))[(1:length(aggr_obs$strat)*2) - 1]
+      aggr_obs$strat2 <- unlist(strsplit(as.character(aggr_obs$strat), ', '))[(1:length(aggr_obs$strat)*2)]
+    }
+  }
+  # data combined and handed off to separate plotting function
+  if(verbose) {
+    message('Creating plot...')
+  }
+  vpc_db <- list(sim = sim,
+                 vpc_dat = vpc_dat,
+                 smooth = smooth,
+                 stratify = stratify,
+                 stratify_original = stratify_original,
+                 stratify_color = stratify_color,
+                 aggr_obs = aggr_obs,
+                 obs = obs,
+                 bins = bins,
+                 facet = facet,
+                 type = 'continuous')
+  if(facet_names == FALSE) {
+    datasets <- c('vpc_dat', 'obs', 'sim', 'aggr_obs')
+    for (i in seq(datasets)) {
+      for (j in seq(vpc_db$stratify)) {
+        vpc_db[[datasets[i]]]$strat <- as.factor(gsub(paste0(vpc_db$stratify[j],'='), '', vpc_db[[datasets[i]]]$strat))
+      }
+    }
+  }
+  vpc_db
+}
