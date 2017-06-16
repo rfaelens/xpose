f742fec7fc57e586646613a5599bd941571ee09f
Benjamin <guiastrennec@gmail.com>
2017-06-16 17:41:28 +0200

    added tests for vpcs

---
+++ tests/testthat/test-vpc.R
@@ -0,0 +1,78 @@
+context('Check VPCs')
+
+# Define files to be tested -----------------------------------------------
+psn_cmd_1 <- '/opt/local64/PsN/bin/vpc -seed=221287 -samples=1000 -stratify_on=VAR1,VAR2 -lst=run001.lst run001.mod'
+psn_cmd_2 <- '/opt/local64/PsN/bin/vpc -seed=221287 -samples=1000 -idv=TAD -dv=IDV -lst=run001.lst run001.mod'
+
+# ctrl_special <- vpc_data(vpc_data(xpdb_ex_pk, vpc_opt = vpc_opt_set(n_bins = 3)),
+#                          vpc_type = 'cens', vpc_opt = vpc_opt_set(n_bins = 3, lloq = -2))
+# save(ctrl_special, file = 'ctrl_special.RData', compress = 'xz')
+load('ctrl_special.RData')
+
+# Tests start here --------------------------------------------------------
+test_that("vpc_opt_set works properly", {
+  expect_equal(vpc_opt_set(), 
+               list(bins = 'jenks', n_bins = 'auto', bin_mid = 'mean', pred_corr = NULL, 
+                    pred_corr_lower_bnd = 0, pi = c(0.05, 0.95),  ci = c(0.05, 0.95),  
+                    lloq = NULL,  uloq = NULL, rtte = FALSE, rtte_calc_diff = TRUE, 
+                    events = NULL, kmmc = NULL, reverse_prob = FALSE, as_percentage = TRUE))
+})
+
+test_that("get_psn_vpc_cols works properly", {
+  expect_equal(get_psn_vpc_cols(psn_cmd_1),
+               list(id = 'ID', idv = 'TIME', dv = 'DV', pred = 'PRED'))
+  expect_equal(get_psn_vpc_cols(psn_cmd_2),
+               list(id = 'ID', idv = 'TAD', dv = 'IDV', pred = 'PRED'))
+})
+
+test_that("get_psn_vpc_strat works properly", {
+  expect_equal(get_psn_vpc_strat(psn_cmd_1), c('VAR1', 'VAR2'))
+  expect_null(get_psn_vpc_strat(psn_cmd_2))
+})
+
+
+test_that("vpc_data properly check input", {
+  expect_error(vpc_data())
+  expect_message(vpc_data(xpdb_ex_pk, psn_folder = '.', quiet = FALSE), 
+                 regexp = 'No data table found.')
+})
+
+test_that("vpc_data works properly with xpdb tables", {
+  xpdb_vpc <- vpc_data(xpdb_ex_pk, vpc_opt = vpc_opt_set(n_bins = 3))
+  expect_true(is.xpdb(xpdb_vpc))
+  expect_identical(xpdb_vpc$special, ctrl_special$special[2, ])
+})
+
+test_that("vpc_type categorical works properly", {
+  xpdb_vpc_cens <- vpc_data(xpdb_ex_pk, vpc_type = 'cens', 
+                            vpc_opt = vpc_opt_set(n_bins = 3, lloq = -2))
+  expect_true(is.xpdb(xpdb_vpc_cens))
+  expect_identical(xpdb_vpc_cens$special, ctrl_special$special[1, ])
+})
+
+test_that("vpc plot properly check input", {
+  expect_error(vpc())
+  expect_message(vpc(xpdb = 1, quiet = FALSE), regexp = 'Bad input')
+  expect_message(vpc(xpdb_ex_pk, quiet = FALSE), regexp = 'No vpc data available')
+  expect_message(vpc(ctrl_special, quiet = FALSE), regexp = 'Several vpc data')
+  expect_error(vpc(ctrl_special, vpc_type = 'unknown', quiet = FALSE), 
+               regexp = 'should be one of')
+})
+
+test_that("vpc plot are properly generated", {
+  p_cont  <- vpc(ctrl_special, vpc_type = 'continuous', type = 'alrpt', quiet = FALSE)
+  p_cont2 <- vpc(ctrl_special, vpc_type = 'continuous', facets = ~group)
+  p_cens  <- vpc(ctrl_special, vpc_type = 'censored', smooth = FALSE, facets = 'group', 
+                 type = 'alr', quiet = FALSE)
+  expect_true(is.xpose.plot(p_cont))
+  expect_true(is.xpose.plot(p_cens))
+  expect_equal(class(p_cont$layers[[1]]$geom)[1], 'GeomRibbon')
+  expect_equal(class(p_cont$layers[[2]]$geom)[1], 'GeomLine')
+  expect_equal(class(p_cont$layers[[3]]$geom)[1], 'GeomPoint')
+  expect_equal(class(p_cont$layers[[4]]$geom)[1], 'GeomText')
+  expect_equal(class(p_cont$layers[[5]]$geom)[1], 'GeomRug')
+  expect_equal(class(p_cont$facet)[1], 'FacetNull')
+  expect_equal(class(p_cont2$facet)[1], 'FacetGrid')
+  expect_equal(class(p_cens$layers[[1]]$geom)[1], 'GeomRect')
+  expect_equal(class(p_cens$facet)[1], 'FacetWrap')
+})
