04ecf2e31e586d6ec598c54b922f389dc1c2d6f8
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2017-06-28 09:43:14 +0200

    centralized list of functions to test

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,57 +1,42 @@
 context('Check plot functions')
 
+functions_to_test <- list(dv_vs_pred,
+                           dv_vs_ipred,
+                           dv_vs_idv,
+                           ipred_vs_idv,
+                           pred_vs_idv,
+                           dv_preds_vs_idv,
+                           res_vs_idv,
+                           res_vs_pred,
+                           absval_res_vs_idv,
+                           absval_res_vs_pred,
+                           prm_vs_iteration,
+                           grd_vs_iteration,
+                           ind_plots,
+                           prm_distrib,
+                           eta_distrib,
+                           res_distrib,
+                           cov_distrib,
+                           prm_qq,
+                           eta_qq,
+                           res_qq,
+                           cov_qq)
+
 # Tests start here --------------------------------------------------------
 
 test_that("errors are returned for missing xpdb_ex_pk", {
-  expect_error(dv_vs_pred())
-  expect_error(dv_vs_ipred())
-  expect_error(dv_vs_idv())
-  expect_error(ipred_vs_idv())
-  expect_error(pred_vs_idv())
-  expect_error(dv_preds_vs_idv())
-  expect_error(res_vs_idv())
-  expect_error(res_vs_pred())
-  expect_error(absval_res_vs_idv())
-  expect_error(absval_res_vs_pred())
-  expect_error(prm_vs_iteration())
-  expect_error(grd_vs_iteration())
-  expect_error(ind_plots())
-  expect_error(prm_distrib())
-  expect_error(eta_distrib())
-  expect_error(res_distrib())
-  expect_error(cov_distrib())
-  expect_error(prm_qq())
-  expect_error(eta_qq())
-  expect_error(res_qq())
-  expect_error(cov_qq())
+  purrr::map(functions_to_test, ~expect_error(.x()))
 })
 
 test_that("xpose plot objects are returned with appropriate xpdb_ex_pk", {
-  expect_true(is.xpose.plot(dv_vs_pred(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(dv_vs_ipred(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(dv_vs_idv(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(ipred_vs_idv(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(pred_vs_idv(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(dv_preds_vs_idv(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(res_vs_idv(xpdb_ex_pk)))
+  purrr::map(functions_to_test, ~expect_true(is.xpose.plot(.x(xpdb_ex_pk))))
+  
   expect_true(is.xpose.plot(res_vs_idv(xpdb_ex_pk, res = c('CWRES', 'IWRES'))))
-  expect_true(is.xpose.plot(res_vs_pred(xpdb_ex_pk)))
   expect_true(is.xpose.plot(res_vs_pred(xpdb_ex_pk, res = c('CWRES', 'IWRES'))))
-  expect_true(is.xpose.plot(absval_res_vs_idv(xpdb_ex_pk)))
   expect_true(is.xpose.plot(absval_res_vs_idv(xpdb_ex_pk, res = c('CWRES', 'IWRES'))))
-  expect_true(is.xpose.plot(absval_res_vs_pred(xpdb_ex_pk)))
   expect_true(is.xpose.plot(absval_res_vs_pred(xpdb_ex_pk, res = c('CWRES', 'IWRES'))))
-  expect_true(is.xpose.plot(prm_vs_iteration(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(grd_vs_iteration(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(ind_plots(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(prm_distrib(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(eta_distrib(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(res_distrib(xpdb_ex_pk)))
   expect_true(is.xpose.plot(res_distrib(xpdb_ex_pk, res = c('CWRES', 'IWRES'))))
-  expect_true(is.xpose.plot(cov_distrib(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(prm_qq(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(eta_qq(xpdb_ex_pk)))
-  expect_true(is.xpose.plot(res_qq(xpdb_ex_pk)))
   expect_true(is.xpose.plot(res_qq(xpdb_ex_pk, res = c('CWRES', 'IWRES'))))
-  expect_true(is.xpose.plot(cov_qq(xpdb_ex_pk)))
 })
+
+
