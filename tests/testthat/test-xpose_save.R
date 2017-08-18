73341fba00704075a6c2458cb79a4fc1d5d82278
Benjamin <guiastrennec@gmail.com>
2017-08-18 21:50:01 +0200

    test: updated xpose_save tests

--- tests/testthat/test-xpose_save.R
+++ tests/testthat/test-xpose_save.R
@@ -8,61 +8,60 @@ plot <- dv_vs_ipred(xpdb = xpdb_ex_pk, quiet = TRUE)
 
 # Tests start here --------------------------------------------------------
 
-test_that("errors are returned for bad plot input", {
-  expect_error(xpose_save(plot = NULL))
+test_that('errors are returned for bad plot input', {
+  expect_error(xpose_save(plot = NULL), regexp = 'The `plot` argument is NULL')
 })
 
-test_that("errors are returned for bad filename input", {
-  paths_1 <- file.path(tempdir(), 
-                       paste0('test_plot', c('.abcd', '.bcde', '')))
+test_that('errors are returned for bad filename input', {
+  paths_1 <- file.path(tempdir(), paste0('test_plot', c('.abcd', '.bcde', '')))
   on.exit(unlink(paths_1))
   
   # Unrecognized extension
-  expect_error(xpose_save(plot = plot, filename = paths_1[1]))
+  expect_error(xpose_save(plot = plot, file = paths_1[1]))
   
   # Missing extension
-  expect_error(xpose_save(plot = plot, filename = paths_1[3]))
+  expect_error(xpose_save(plot = plot, file = paths_1[3]))
   
   # Missing filename
-  expect_error(xpose_save(plot = plot, filename = NULL))
+  expect_error(xpose_save(plot = plot, file = NULL))
   
   # Length filename > 1
-  expect_error(xpose_save(plot = plot, filename = paths_1))
+  expect_error(xpose_save(plot = plot, file = paths_1))
 })
 
 
-test_that("common graphical device work properly", {
-  paths_2 <- file.path(tempdir(), 
-                       paste0('test_plot.', c('pdf', 'jpeg', 'png')))
+test_that('common graphical device work properly', {
+  paths_2 <- file.path(tempdir(), paste0('test_plot.', c('pdf', 'jpeg', 'png')))
   on.exit(unlink(paths_2))
   
   expect_false(any(file.exists(paths_2)))
   
   # Test pdf
-  xpose_save(plot = plot, filename = paths_2[1])
+  xpose_save(plot = plot, file = paths_2[1])
   expect_true(file.exists(paths_2[1]))
   
   # Test jpeg
   if (capabilities('jpeg')) {
-    xpose_save(plot = plot, filename = paths_2[2], dpi = 20) # jpeg
+    xpose_save(plot = plot, file = paths_2[2], dpi = 20) # jpeg
     expect_true(file.exists(paths_2[2]))
   }
   
   # Test png
   if (capabilities('png')) {
-    xpose_save(plot = plot, filename = paths_2[3], dpi = 20) # png
+    xpose_save(plot = plot, file = paths_2[3], dpi = 20) # png
     expect_true(file.exists(paths_2[3]))
   }
 })
 
-test_that("template filenames and auto file extension work properly", {
+test_that('template filenames and auto file extension work properly', {
   paths_3 <- file.path(tempdir(), 'run001_dv_vs_ipred.pdf') 
   on.exit(unlink(paths_3))
   
   expect_false(file.exists(paths_3))
   
   xpose_save(plot = plot, 
-             filename = file.path(tempdir(), '@run_@plotfun'), 
+             file = '@run_@plotfun',
+             dir = tempdir(),
              device = 'pdf')
   expect_true(file.exists(paths_3))
 })
