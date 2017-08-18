f66612e5f2350a3eb57979dfac7b0882b97a70d3
Benjamin <guiastrennec@gmail.com>
2017-08-18 21:59:03 +0200

    tests: updated set_vars tests

--- tests/testthat/test-set_vars.R
+++ tests/testthat/test-set_vars.R
@@ -1,14 +1,14 @@
 context('Check set_vars')
 
 # Tests start here --------------------------------------------------------
-test_that("input is check properly", {
+test_that('input is check properly', {
   expect_error(set_var_types())
   expect_error(set_var_units())
   expect_error(set_var_types(xpdb_ex_pk, problem = 99), regexp = 'not found in model')
   expect_error(set_var_labels(xpdb_ex_pk, problem = 99), regexp = 'not found in model')
 })
 
-test_that("set_vars_type works properly", {
+test_that('set_vars_type works properly', {
   xpdb_2 <- set_var_types(xpdb_ex_pk, problem = NULL, 
                           idv = 'PRED', catcov = c('SS', 'II'), fake = 'HELLO')
   expect_true(is.xpdb(xpdb_2))
@@ -19,7 +19,7 @@ test_that("set_vars_type works properly", {
   expect_equal(class(xpdb_2$data$data[[1]]$SS), 'factor')
 })
 
-test_that("set_vars_units and set_vars_label works properly", {
+test_that('set_vars_units and set_vars_label works properly', {
   xpdb_3 <- set_var_units(xpdb_ex_pk, problem = NULL, 
                           CL = 'L/h', V = 'L', fake = 'HELLO')
   expect_true(is.xpdb(xpdb_3))
