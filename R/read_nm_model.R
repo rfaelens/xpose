efb1eec27721803f7cd2eecfe2f5d903621304db
Benjamin <guiastrennec@gmail.com>
2017-08-22 23:11:35 +0200

    read_nm_model: fixed cran check warning

--- R/read_nm_model.R
+++ R/read_nm_model.R
@@ -136,8 +136,8 @@ read_nm_model <- function(runno   = NULL,
   # Remove na values and output
   tidyr::replace_na(model, replace = list(code = '', comment = '')) %>% 
     dplyr::select(dplyr::one_of('problem', 'level', 'subroutine', 'code', 'comment')) %>% 
-    dplyr::mutate(problem = as.integer(as.character(problem)),
-                  level   = as.integer(as.character(level))) %>% 
+    dplyr::mutate(problem = as.integer(.$problem),
+                  level   = as.integer(.$level)) %>% 
     structure(file     = basename(full_path),
               dir      = dirname(full_path),
               software = 'nonmem',
