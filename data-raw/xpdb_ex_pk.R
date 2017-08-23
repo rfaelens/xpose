6b0a44c0c04299ecae3fa07addfee602a4a1a8ca
Benjamin <guiastrennec@gmail.com>
2017-08-23 16:28:46 +0200

    xpdb_ex_pk: added coded to generate example

---
+++ data-raw/xpdb_ex_pk.R
@@ -0,0 +1,19 @@
+#' Generate xpdb example for the xpose package
+#' 
+#' @description Generate the xpdb_ex_pk.rda object when change in internal xpose 
+#' structure is made.
+#' @usage Set working directory to the xpose project directory. And run the whole script.
+#' 
+library(xpose)
+
+# Generate xpdb
+xpdb_ex_pk <- xpose_data(runno = '001', dir = 'inst/extdata/', quiet = FALSE) %>% 
+  set_var_types(idv = 'TAD')
+
+# Overwrite directory paths
+xpdb_ex_pk$summary$value[xpdb_ex_pk$summary$label == 'dir'] <- 'analysis/models/pk/'
+xpdb_ex_pk$options$dir <- 'analysis/models/pk/'
+attr(xpdb_ex_pk$code, 'dir') <- 'analysis/models/pk/'
+
+# Export rda to data folder
+save(xpdb_ex_pk, file = 'data/xpdb_ex_pk.rda', compress = 'xz')
