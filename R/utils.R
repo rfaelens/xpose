bcd536885193b792c45a8fb38cc1cd85e85fe3f6
Benjamin <guiastrennec@gmail.com>
2017-09-11 03:28:18 +0200

    docs: continued internal functions documentation

--- R/utils.R
+++ R/utils.R
@@ -2,58 +2,147 @@
 #' @export
 dplyr::filter
 
+
 #' @importFrom dplyr mutate
 #' @export
 dplyr::mutate
+
 
 #' @importFrom purrr %>%
 #' @export
 purrr::`%>%`
 
+
 #' @importFrom ggforce facet_wrap_paginate
 #' @export
 ggforce::facet_wrap_paginate
+
 
 #' @importFrom ggforce facet_grid_paginate
 #' @export
 ggforce::facet_grid_paginate
 
-# Reports whether x is an xpose theme object
+
+#' Test for xpose_theme class
+#' 
+#' @description Reports whether x is an `xpose_theme` object
+#' 
+#' @param x An object to be tested.
+#' 
+#' @return Logical value, `TRUE` for `xpose_theme` class 
+#' and `FALSE` otherwise.
+#' 
+# @keywords internal
+# @export
 is.xp.theme <- function(x) {
   inherits(x, 'xpose_theme')
 }
 
-# Reports whether x is an xpose data object
+
+#' Test for xpose_data class
+#' 
+#' @description Reports whether x is an `xpose_data` object
+#' 
+#' @param x An object to be tested.
+#' 
+#' @return Logical value, `TRUE` for `xpose_data` class 
+#' and `FALSE` otherwise.
+#' 
+# @keywords internal
+# @export
 is.xpdb <- function(x) {
   inherits(x, 'xpose_data')
 }
 
-# Reports whether x is a nm_model object
+
+#' Test for nm_model class
+#' 
+#' @description Reports whether x is a `nm_model` object
+#' 
+#' @param x An object to be tested.
+#' 
+#' @return Logical value, `TRUE` for `nm_model` class 
+#' and `FALSE` otherwise.
+#' 
+# @keywords internal
+# @export
 is.nm.model <- function(x) {
   inherits(x, 'nm_model')
 }
 
-# Reports whether x is a nm.table.list object
+#' Test for nm_table_list class
+#' 
+#' @description Reports whether x is a `nm_table_list` object
+#' 
+#' @param x An object to be tested.
+#' 
+#' @return Logical value, `TRUE` for `nm_table_list` class 
+#' and `FALSE` otherwise.
+#' 
+# @keywords internal
+# @export
 is.nm.table.list <- function(x) {
   inherits(x, 'nm_table_list')
 }
 
-# Adds nm_table_list attribute to an object
+
+#' Convert an object to `nm_table_list` class
+#' 
+#' @description Adds `nm_table_list` attribute to an object
+#' 
+#' @param x An object to be modified.
+#' 
+#' @return x with `nm_table_list` class.
+#' 
+# @keywords internal
+# @export
 as.nm.table.list <- function(x) {
   structure(x, class = c('nm_table_list', class(x)))
 }
 
-# Reports whether x is an xpose_plot object
+
+#' Test for xpose_plot class
+#' 
+#' @description Reports whether x is a `xpose_plot` object
+#' 
+#' @param x An object to be tested.
+#' 
+#' @return Logical value, `TRUE` for `xpose_plot` class 
+#' and `FALSE` otherwise.
+#' 
+# @keywords internal
+# @export
 is.xpose.plot <- function(x) {
   inherits(x, 'xpose_plot')
 }
 
-# Check whether x is a formula
+
+#' Test for formula class
+#' 
+#' @description Reports whether x is a `formula` object
+#' 
+#' @param x An object to be tested.
+#' 
+#' @return Logical value, `TRUE` for `formula` class 
+#' and `FALSE` otherwise.
+#' 
+# @keywords internal
+# @export
 is.formula <- function(x) {
   inherits(x, 'formula')
 }
 
-# Convert seconds to hh:mm:ss
+
+#' Time converter
+#' 
+#' @description Convert seconds to hh:mm:ss
+#' 
+#' @param x A string ('character' or 'numeric') of time in seconds.
+#' 
+#' @return A string of time in `hh:mm:ss`.
+#' 
+# @keywords internal
+# @export
 as.ctime <- function(x) {
   x <- round(as.numeric(x), 0)
   sprintf("%02d:%02d:%02d", 
@@ -62,12 +151,35 @@ as.ctime <- function(x) {
           x %% 60)
 }
 
-# Message function with quiet option (from Ron Keizer)
+
+#' Message function
+#' 
+#' @description Message function with quiet option inspired from `ronkeizer/vpc`.
+#' 
+#' @param txt A string for the message.
+#' @param quiet Should messages be displayed to the console.
+#' 
+#' @return Silent when quiet is `TRUE` or a message is quiet is `FALSE`.
+#' 
+# @keywords internal
+# @export
 msg <- function(txt, quiet = TRUE) {
   if (!quiet) message(txt)
 }
 
-# Generate file paths
+
+#' Generate clean file paths
+#' 
+#' @description Wrapper around `file.path` that cleans trailing forward 
+#' slash and missing `dir`.
+#' 
+#' @param dir A string or vector of strings containing the directory path.
+#' @param file A file name or vector containing the file names.
+#' 
+#' @return A string or vector of string of the full file path.
+#' 
+# @keywords internal
+# @export
 file_path <- function(dir, file) {
   if (is.null(dir)) return(file) 
   
@@ -76,7 +188,18 @@ file_path <- function(dir, file) {
   file.path(dir, file)
 }
 
-# Get file extension
+#' Get file extension
+#' 
+#' @description Extract file extension from the filename string.
+#' 
+#' @param x A string or vector of strings containing the filenames with the extension.
+#' @param dot Logical, if `TRUE` the returned value will contain the 
+#' dot (e.g `.mod`) else only the extension itself will be returned (e.g. `mod`).
+#' 
+#' @return A string or vector of string of the file(s) extension.
+#' 
+# @keywords internal
+# @export
 get_extension <- function(x, dot = TRUE) {
   x <- stringr::str_extract(x, '\\.[[:alnum:]]+$')
   x[is.na(x)] <- ''
@@ -84,12 +207,34 @@ get_extension <- function(x, dot = TRUE) {
   x
 }
 
-# Get file extension
+
+#' Generate extension string
+#' 
+#' @description Generate consistent extension strings by adding dot 
+#' prefix whenever necessary.
+#' 
+#' @param x A string or vector of strings containing the extension to be standardized.
+#' 
+#' @return A string or vector of strings of extension(s).
+#' 
+# @keywords internal
+# @export
 make_extension <- function(x) {
   dplyr::if_else(!stringr::str_detect(x, '^\\..+'), stringr::str_c('.', x), x)
 }
 
-# Update file extension
+
+#' Update file extension
+#' 
+#' @description Change the extension of a file.
+#' 
+#' @param x A string or vector of strings containing the file name to be modified.
+#' @param ext A string or vector of strings containing the name of the new extension(s).
+#' 
+#' @return A string or vector of strings of file name(s).
+#' 
+# @keywords internal
+# @export
 update_extension <- function(x, ext) {
   stringr::str_replace(x, '\\.[[:alnum:]]+$', ext)
 }
