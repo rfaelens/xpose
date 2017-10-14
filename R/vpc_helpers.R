db32d76cddbb21eaffe4e9f12fcb9394bd7d90c2
Benjamin <guiastrennec@gmail.com>
2017-10-14 11:48:08 +0200

    vpc_helpers: improved detection of user input in vpc_opt, changed pi and ci default to 95%, removed get_psn_vpc_cols, added psn_vpc_parser now using version_and_option_info.txt instead of command.txt, now also detects PsN binning

--- R/vpc_helpers.R
+++ R/vpc_helpers.R
@@ -35,7 +35,7 @@
 vpc_opt <- function(bins                = 'jenks',
                     n_bins              = 'auto',
                     bin_mid             = 'mean',
-                    pred_corr           = NULL, 
+                    pred_corr           = FALSE, 
                     pred_corr_lower_bnd = 0,
                     pi                  = c(0.025, 0.975), 
                     ci                  = c(0.025, 0.975), 
@@ -51,25 +51,8 @@ vpc_opt <- function(bins                = 'jenks',
   list(bins = bins, n_bins = n_bins, bin_mid = bin_mid, pred_corr = pred_corr, 
        pred_corr_lower_bnd = pred_corr_lower_bnd, pi = pi, ci = ci, lloq = lloq, 
        uloq = uloq, rtte = rtte, rtte_calc_diff = rtte_calc_diff, events = events, 
-       kmmc = kmmc, reverse_prob = reverse_prob, as_percentage = as_percentage)
-}
-
-
-# Get columns info from PsN
-get_psn_vpc_cols <- function(psn_cmd) {
-  if (stringr::str_detect(psn_cmd, '-idv')) {
-    idv <- stringr::str_match(psn_cmd, '-idv=\\s*([^\\s]+)')[1, 2]
-  } else {
-    idv <- 'TIME'
-  }
-  
-  if (stringr::str_detect(psn_cmd, '-dv')) {
-    dv <- stringr::str_match(psn_cmd, '-dv=\\s*([^\\s]+)')[1, 2]
-  } else {
-    dv <- 'DV'
-  }
-  
-  list(id = 'ID', idv = idv, dv = dv, pred = 'PRED')
+       kmmc = kmmc, reverse_prob = reverse_prob, as_percentage = as_percentage, 
+       usr_call = names(match.call()[-1]))
 }
 
 
@@ -81,4 +64,81 @@ get_psn_vpc_strat <- function(psn_cmd) {
       stringr::str_split(',') %>% 
       purrr::flatten_chr()
   }
+}
+
+# Gets vpc options from PsN directory
+psn_vpc_parser <- function(xpdb, psn_folder, opt, quiet) {
+  
+  psn_folder <- parse_title(string = psn_folder, xpdb = xpdb, quiet = quiet,
+                            problem = default_plot_problem(xpdb))
+  
+  if (!dir.exists(psn_folder)) {
+    stop('The `psn_folder`:', psn_folder, ' could not be found.', call. = FALSE)
+  }
+  msg(' - Importing PsN generated data', quiet)
+  
+  if (!dir.exists(file_path(psn_folder, 'm1')) &
+      file.exists(file_path(psn_folder, 'm1.zip'))) {
+    msg(' - Unziping PsN m1 folder', quiet)
+    utils::unzip(zipfile = file_path(psn_folder, 'm1.zip'), 
+                 exdir   = file_path(psn_folder, ''))
+    unzip <- TRUE
+  } else {
+    unzip <- FALSE 
+  }
+  obs_data <- read_nm_tables(file = dir(file_path(psn_folder, 'm1'), pattern = 'original.npctab')[1],
+                             dir = file.path(psn_folder, 'm1'), quiet = TRUE)
+  sim_data <- read_nm_tables(file = dir(file_path(psn_folder, 'm1'), pattern = 'simulation.1.npctab')[1],
+                             dir = file.path(psn_folder, 'm1'), quiet = TRUE)
+  if (unzip) unlink(x = file_path(psn_folder, 'm1'), recursive = TRUE, force = TRUE)
+  
+  # Getting multiple options form the psn command
+  if (file.exists(file_path(psn_folder, 'version_and_option_info.txt'))) {
+    # Get list of options from PsN
+    psn_opt <- readr::read_lines(file = file_path(psn_folder, 'version_and_option_info.txt')) 
+    psn_cmd <- psn_opt[which(stringr::str_detect(psn_opt, '^Command:')) + 1]
+    psn_opt <- dplyr::data_frame(raw = psn_opt[stringr::str_detect(psn_opt,'^-')]) %>% 
+      tidyr::separate_(col = 'raw', into = c('arg', 'value'), sep = '=') %>% 
+      dplyr::mutate(arg = stringr::str_replace(.$arg, '^-', ''))
+    
+    # Sets obs and sim cols
+    obs_cols <- list(id = 'ID', idv = psn_opt$value[psn_opt$arg == 'idv'], 
+                     dv = psn_opt$value[psn_opt$arg == 'dv'], pred = 'PRED')  
+    sim_cols <- obs_cols
+    
+    # Sets additional options
+    if (!any(opt$usr_call == 'pred_corr')) {
+      pred_corr <- as.logical(as.numeric(psn_opt$value[psn_opt$arg == 'predcorr']))
+      if (!is.na(pred_corr)) opt$pred_corr <- pred_corr
+    }
+    if (!any(opt$usr_call == 'lloq')) {
+      lloq <- as.numeric(psn_opt$value[psn_opt$arg == 'lloq'])
+      if (length(lloq) > 0 && !is.na(lloq)) opt$lloq <- lloq
+    }
+    if (!any(opt$usr_call == 'uloq')) {
+      uloq <- as.numeric(psn_opt$value[psn_opt$arg == 'uloq'])
+      if (length(uloq) > 0 && !is.na(uloq)) opt$uloq <- uloq
+    }
+  } else {
+    msg(' - PsN file `version_and_option_info.txt` not found. Using default options.', quiet)
+    obs_cols <- c(id = 'ID', idv = 'TIME', dv = 'DV', pred = 'PRED')
+    sim_cols <- obs_cols
+  }
+  if (file.exists(file_path(psn_folder, 'vpc_bins.txt')) && !any(opt$usr_call == 'bins')) {
+    psn_bins <- readr::read_lines(file = file_path(psn_folder, 'vpc_bins.txt')) %>% 
+      .[nchar(.) > 0] %>% 
+      tail(n = 1) %>% 
+      stringr::str_replace('^.+=', '') %>% 
+      {stringr::str_split(., ':')[[1]]} %>% 
+      {stringr::str_split(., ',')} %>% 
+      purrr::map(~as.numeric(.x))
+    
+    opt$bins <- psn_bins[[1]] # vpc does not handle panel based binning yet so take the first one
+    msg(c(' - Using PsN-defined binning', ifelse(length(unique(psn_bins)) == 1 , '',
+                                              '. Only a single bin_array (i.e. first) can be used by xpose.')), quiet)
+  }
+  
+  list(obs_data = obs_data, obs_cols = obs_cols, 
+       sim_data = sim_data, sim_cols = sim_cols,
+       opt = opt, psn_folder = psn_folder, psn_cmd = psn_cmd)
 }
