bcae8937ca128946e35f59f937c3059f1dc9babe
Benjamin <guiastrennec@gmail.com>
2017-11-01 00:49:03 +0100

    get_prm: removed print method and added prm_table a dedicated function for nice output to the console

---
+++ R/prm_table.R
@@ -0,0 +1,57 @@
+#' Display a parameter table to the console
+#' 
+#' @description Display parameter estimates from an xpdb object to the console.
+#' 
+#' @param xpdb An \code{xpose_data} object from which the model output file data will be extracted.
+#' @param .problem The problem to be used, by default returns the last one for each file.
+#' @param .subprob The subproblem to be used, by default returns the last one for each file.
+#' @param .method The estimation method to be used, by default returns the last one for each file
+#' @param digits The number of significant digits to be displayed.
+#' @param show_all Logical, whether the 0 fixed off-diagonal elements should be removed from the output.
+#' @param quiet Logical, if \code{FALSE} messages are printed to the console.
+#' 
+#' @return A tibble for single problem/subprob or a named list for multiple problem|subprob.
+#' @examples
+#' # Display to the console
+#' prm_table(xpdb_ex_pk, .problem = 1)
+#' 
+#' @export
+prm_table <- function(xpdb,
+                      .problem = NULL, 
+                      .subprob = NULL, 
+                      .method  = NULL,
+                      digits   = 4,
+                      show_all = FALSE) {
+  
+  x <- get_prm(xpdb = xpdb, .problem = .problem, .subprob = .subprob, 
+               .method = .method, digits = digits, show_all = show_all, quiet = TRUE)
+  
+  cat('\nThe relative standard errors for omega and sigma are reported on the approximate
+standard deviation scale (SE/variance estimate)/2.\n')
+  
+  # Convert single prm_df to list
+  if (dplyr::is.tbl(x)) x <- list(x)
+  
+  # Generate output to console
+  purrr::map(.x = x, function(prm) {
+    prm_attr <- attributes(prm)
+    header <- dplyr::data_frame(name = 'Parameter', label = 'Label', 
+                                value = 'Value', rse = 'RSE', fixed = '   ')
+    
+    cat('\nEstimates for $prob no.', prm_attr$problem, 
+        ', subprob no.', prm_attr$subprob, ', method ', prm_attr$method, '\n', sep = '')
+    
+    prm %>% 
+      dplyr::mutate_all(.funs = 'as.character') %>% 
+      dplyr::mutate(fixed = ifelse(.$fixed, 'fix', '   ')) %>% 
+      {dplyr::bind_rows(header, .)} %>% 
+      dplyr::mutate(name  = stringr::str_pad(.$name, max(nchar(.$name)), 'right'),
+                    label = stringr::str_pad(.$label, max(nchar(.$label)), 'right'),
+                    value = stringr::str_pad(.$value, max(nchar(.$value)), 'right'),
+                    rse   = ifelse(is.na(.$rse), ' - ', .$rse)) %>%
+      dplyr::mutate(string = stringr::str_c('', .$name, .$label, .$value, .$fixed, .$rse, sep = ' ')) %>% 
+      {purrr::flatten_chr(.[,'string'])} %>% 
+      cat(sep = '\n')
+  }) %>% 
+    invisible()
+}
