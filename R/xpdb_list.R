c14753106f8d6c7c10ef92a381e62bf696d844ca
Benjamin <guiastrennec@gmail.com>
2017-11-01 01:23:37 +0100

    list_xxx: added new functions list_data, list_files and list_special to help understanding the internal structure of the xpdb

---
+++ R/xpdb_list.R
@@ -0,0 +1,49 @@
+#' List available datasets
+#'
+#' @description Function providing a detailed listing of all available datasets in an xpdb object.
+#' 
+#' @param xpdb An \code{xpose_data} object to be evaluated
+#' 
+#' @examples
+#' # List output tables data
+#' list_data(xpdb_ex_pk)
+#' 
+#' # List output files data
+#' list_files(xpdb_ex_pk)
+#' 
+#' # List special data
+#' xpdb_ex_pk %>% 
+#' vpc_data(quiet = TRUE) %>% 
+#' list_special()
+#' 
+#' @export
+list_data <- function(xpdb) {
+  check_xpdb(xpdb, check = 'data')
+  xpdb$data %>% 
+    dplyr::select(dplyr::one_of('problem', 'simtab', 'data', 'modified')) %>% 
+    {utils::capture.output(print(.))} %>% 
+    {c('Data:', .[-1])} %>% 
+    cat(sep = '\n')
+}
+
+#' @name list_data
+#' @export
+list_files <- function(xpdb) {
+  check_xpdb(xpdb, check = 'files')
+  xpdb$files %>% 
+    dplyr::select(dplyr::one_of('name', 'extension', 'problem', 'subprob', 'method', 'data', 'modified')) %>% 
+    {utils::capture.output(print(.))} %>% 
+    {c('Files:', .[-1])} %>% 
+    cat(sep = '\n')
+}
+
+#' @name list_data
+#' @export
+list_special <- function(xpdb) {
+  check_xpdb(xpdb, check = 'special')
+  xpdb$special %>% 
+    dplyr::select(dplyr::one_of('problem', 'method', 'type', 'data', 'modified')) %>% 
+    {utils::capture.output(print(.))} %>% 
+    {c('Specials:', .[-1])} %>% 
+    cat(sep = '\n')
+}
