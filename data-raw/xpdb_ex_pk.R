d68259839e8aead1b5ad337a550e51a51f522cb9
unknown <guiastrennec@gmail.com>
2018-08-15 22:38:39 +0200

    xpdb_ex_pk: fixed class

--- data-raw/xpdb_ex_pk.R
+++ data-raw/xpdb_ex_pk.R
@@ -13,6 +13,6 @@ xpdb_ex_pk <- xpose_data(runno = '001', dir = 'inst/extdata/', quiet = FALSE)
 xpdb_ex_pk$summary$value[xpdb_ex_pk$summary$label == 'dir'] <- 'analysis/models/pk/'
 xpdb_ex_pk$options$dir <- 'analysis/models/pk/'
 attr(xpdb_ex_pk$code, 'dir') <- 'analysis/models/pk/'
-
+xpdb_ex_pk <- xpose::as.xpdb(xpdb_ex_pk)
 # Export rda to data folder
 save(xpdb_ex_pk, file = 'data/xpdb_ex_pk.rda', compress = 'xz')
